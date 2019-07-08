a629fe2f015a955536bf15fccf89ce7db9786586
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-07-08 11:38:52 +0200

    update

--- src/tmp/TestReadZip.java
+++ src/tmp/TestReadZip.java
@@ -1,52 +1,36 @@
 package tmp;
 
+import com.google.gson.Gson;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import util.FileUtils;
 import util.JSchUtils;
 
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
 public class TestReadZip {
+
     public static void main(String[] args) throws Exception {
         ZipArchiveInputStream zipInput =
-                new ZipArchiveInputStream(JSchUtils.getFileInputStreamFromServer("/GW/D5data-10/hvthinh/BriQ-TableM/health.zip"));
+                new ZipArchiveInputStream(new FileInputStream("/GW/D5data-10/hvthinh/BriQ-TableM/politics.zip"));
 
         ZipArchiveEntry entry;
-//        PrintWriter out = FileUtils.getPrintWriter("/home/hvthinh/datasets/BriQ-tableM/health_filtered.gz", "UTF-8");
-        int nDoc = 0;
-        int nPart = 0;
+
+
+        PrintWriter out = FileUtils.getPrintWriter("/GW/D5data-10/hvthinh/BriQ-TableM/politics_combined.gz", "UTF-8");
+        Gson gson = new Gson();
         while ((entry = zipInput.getNextZipEntry()) != null) {
             if (entry.isDirectory()) {
                 continue;
             }
-            ++nPart;
             System.out.println("Processing: " + entry.getName());
             for (String line : new FileUtils.LineStream(zipInput, StandardCharsets.UTF_8, false)) {
-                ++nDoc;
-//                try {
-//                    JSONObject json = new JSONObject(line);
-//                    if (json.has("fullText")) {
-//                        json.remove("fullText");
-//                    }
-//                    if (json.has("termSet")) {
-//                        json.remove("termSet");
-//                    }
-//                    if (json.has("relation")) {
-//                        json.remove("relation");
-//                    }
-//                    if (json.has("title") && !json.getString("title").isEmpty()) {
-//                        out.println(json.toString());
-//                    }
-//                } catch (Exception e) {
-//                    System.err.println(line);
-//                }
+                out.println(line);
             }
         }
-//        out.close();
-        System.out.println("-------------------------------------");
-        System.out.println("nPart: " + nPart);
-        System.out.println("nDoc: " + nDoc);
+        out.close();
         JSchUtils.stop();
     }
 }
