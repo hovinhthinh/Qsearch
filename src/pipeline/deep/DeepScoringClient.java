5d8a3bf5c6f6341f2adae37ef31266d805518fec
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-09-13 16:38:27 +0200

    update

---
+++ src/pipeline/deep/DeepScoringClient.java
@@ -0,0 +1,72 @@
+package pipeline.deep;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class DeepScoringClient {
+
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+    private Process p = null;
+
+    public DeepScoringClient() {
+        try {
+            String[] cmd = new String[]{
+                    "/bin/sh", "-c",
+                    "python3 -u predict.py",
+            };
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            pb.directory(new File("./deep"));
+            p = pb.start();
+            in = new BufferedReader(new InputStreamReader(p.getInputStream(), StandardCharsets.UTF_8));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(p.getOutputStream(), StandardCharsets.UTF_8)));
+            String str;
+            while (!(str = in.readLine()).equals("__ready_to_predict__")) {
+                System.out.println(str);
+            }
+            System.out.println(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DeepScoringClient client = new DeepScoringClient();
+        System.out.println(client.getScore(Arrays.asList("team", "stadium", "dog"), "capacity"));
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            in.close();
+        } catch (Exception e) {
+        }
+        try {
+            out.close();
+        } catch (Exception e) {
+        }
+        try {
+            p.destroy();
+        } catch (Exception e) {
+        }
+        super.finalize();
+    }
+
+    // return: <optimal position>\t<score>
+    public synchronized String getScore(List<String> entitiesDesc, String quantityDesc) {
+        JSONObject o = new JSONObject().put("quantity_desc", quantityDesc).put("entities_desc", new JSONArray(entitiesDesc));
+        out.println(o.toString());
+        out.flush();
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
