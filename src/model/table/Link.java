5f0958d1f8dbfb47337a53977a3e185b6da6aac6
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-09-16 13:55:24 +0200

    update

--- src/model/table/Link.java
+++ src/model/table/Link.java
@@ -1,7 +1,7 @@
 package model.table;
 
 public class Link {
-    public String target; // target
+    public String target; // target # e.g., YAGO:Cristiano_Ronaldo
     public String text; // anchor text
 }
 
