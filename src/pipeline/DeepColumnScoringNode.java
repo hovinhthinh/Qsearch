a00b0004be3ab6b8b66b8f74ace0fdfc1adde8d1
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-11-25 16:11:48 +0100

    update

--- src/pipeline/DeepColumnScoringNode.java
+++ src/pipeline/DeepColumnScoringNode.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 // Link quantity columns to entity columns, return false if there is no quantity column.
-// TODO: currently link numeric columns to both entity columns and other columns, need to link to entity columns only.
 public class DeepColumnScoringNode implements TaggingNode {
     public static final Logger LOGGER = Logger.getLogger(DeepColumnScoringNode.class.getName());
     public static final int MIN_MAX_INFERENCE = 0;
