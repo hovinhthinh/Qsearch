7c347573dc5960ac359b463ba8bb2c3ce1857817
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-11-27 15:10:09 +0100

    update

--- run_parallel.sh
+++ run_parallel.sh
@@ -6,12 +6,12 @@
 np=$(($1-1))
 echo "Split data"
 args="$3 $1"
-mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="util.FileSplitter" -Dexec.args="$args" 1>>$4.out 2>>$4.err
+mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="util.FileSplitter" -Dexec.args="$args"
 
 echo "Run in parallel"
 for i in $(seq -f "%02g" 0 ${np}); do
 args="$3.part$i.gz $4.part$i.gz ${@:5}"
-export MAVEN_OPTS="-Xmx8G" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args" 1>>$4.out 2>>$4.err &
+export MAVEN_OPTS="-Xmx8G" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args" 1>$4.part$i.out 2>$4.part$i.err &
 done
 
 wait
@@ -19,5 +19,8 @@ wait
 echo "Combine result"
 zcat $4.part*.gz | gzip > $4
 echo "Clean"
-rm $3.part*.gz $4.part*.gz
+# Remove input parts.
+rm $3.part*.gz
+# Remove output parts.
+rm $4.part*.gz
 
