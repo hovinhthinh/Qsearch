cb5a0b937802030cfe10d709f8dde99c4a43c75d
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-12-13 15:53:13 +0100

    update

--- src/storage/table/ElasticSearchTableImport.java
+++ src/storage/table/ElasticSearchTableImport.java
@@ -2,6 +2,7 @@ package storage.table;
 
 import com.google.gson.Gson;
 import config.Configuration;
+import data.tablem.TABLEM;
 import model.table.Table;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,6 +10,7 @@ import util.FileUtils;
 import util.HTTPRequest;
 import util.SelfMonitor;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class ElasticSearchTableImport {
@@ -16,15 +18,55 @@ public class ElasticSearchTableImport {
     public static final String ES_HOST = Configuration.get("storage.elasticsearch.address");
     public static final String TABLE_INDEX = Configuration.get("storage.elasticsearch.table_index");
     public static final String TABLE_TYPE = Configuration.get("storage.elasticsearch.table_type");
-    public static final int BATCH_SIZE = 1024 * 8;
+    public static final int BATCH_SIZE = 1024;
     public static ArrayList<String> bulks = new ArrayList<>();
+
+    static class TableIndex {
+        private static final Gson GSON = new Gson();
+        Table table;
+        String pageContent, caption, pageTitle, tableText;
+
+        public String toESBulkContent() {
+            return new JSONObject()
+                    .put("pageContent", pageContent)
+                    .put("caption", caption)
+                    .put("pageTitle", pageTitle)
+                    .put("tableText", tableText)
+                    .put("json", GSON.toJson(table))
+                    .toString();
+        }
+    }
 
     public static String deleteIndex() {
         return HTTPRequest.DELETE(PROTOCOL + "://" + ES_HOST + "/" + TABLE_INDEX, null);
     }
 
     public static String createIndex() {
-        String body = "{\"mappings\":{\"" + TABLE_TYPE + "\":{\"properties\":{}}}}";
+//        {
+//            "mappings": {
+//                "table": {
+//                    "properties": {
+//                        "json": { # this is in the model.table.Table format.
+//                            "index": false,
+//                            "type": "text"
+//                        },
+//                        "pageContent": {
+//                            "type": "text"
+//                        },
+//                        "caption": {
+//                            "type": "text"
+//                        },
+//                        "pageTitle": {
+//                            "type": "text"
+//                        },
+//                        "tableText": {
+//                            "type": "text"
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        String body = "{\"mappings\":{\"" + TABLE_TYPE + "\":{\"properties\":{\"json\":{\"index\":false,\"type\":\"text\"},\"pageContent\":{\"type\":\"text\"},\"caption\":{\"type\":\"text\"},\"pageTitle\":{\"type\":\"text\"},\"tableText\":{\"type\":\"text\"}}}}}";
         return HTTPRequest.PUT(PROTOCOL + "://" + ES_HOST + "/" + TABLE_INDEX, body);
     }
 
@@ -45,12 +87,12 @@ public class ElasticSearchTableImport {
         }
     }
 
-    private static boolean bulk(String tid, String json) {
+    private static boolean bulk(TableIndex tableIndex) {
         try {
-            JSONObject index = new JSONObject().put("index", new JSONObject().put("_id", tid));
-            JSONObject body = new JSONObject().put("json", json);
+            JSONObject index = new JSONObject().put("index", new JSONObject().put("_id", tableIndex.table._id));
+            String body = tableIndex.toESBulkContent();
             bulks.add(index.toString());
-            bulks.add(body.toString());
+            bulks.add(body);
         } catch (JSONException e) {
             e.printStackTrace();
             return false;
@@ -62,7 +104,7 @@ public class ElasticSearchTableImport {
     }
 
     // args: <table file>
-    public static boolean importTables(String input) {
+    private static boolean importTables(String input) {
         System.out.println("Import tables from: " + input);
 
         SelfMonitor monitor = new SelfMonitor("ImportTable", -1, 10);
@@ -70,30 +112,57 @@ public class ElasticSearchTableImport {
 
         Gson gson = new Gson();
         for (String line : FileUtils.getLineStream(input)) {
-            Table t = gson.fromJson(line, Table.class);
-            String id = t._id;
-            if (id == null) {
+            TableIndex tableIndex = gson.fromJson(line, TableIndex.class);
+            if (tableIndex.table._id == null) {
                 throw new RuntimeException("null table id");
             }
-            if (!bulk(id, line)) {
+            if (!bulk(tableIndex)) {
                 monitor.forceShutdown();
                 return false;
             }
             monitor.incAndGet();
         }
-        monitor.forceShutdown();
         if (bulks.size() > 0) {
             return callBulk();
         }
+        monitor.forceShutdown();
         return true;
     }
 
+    // /GW/D5data-11/hvthinh/TABLEM/all/all+id.shuf.gz /GW/D5data-11/hvthinh/TABLEM/all/all+id.shuf_to_be_indexed.gz
+    public static void processTableData(String[] args) {
+        PrintWriter out = FileUtils.getPrintWriter(args[1], "UTF-8");
+        FileUtils.LineStream stream = FileUtils.getLineStream(args[0], "UTF-8");
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(deleteIndex());
-        System.out.println(createIndex());
-        System.out.println("Importing tables:");
-        System.out.println(importTables(null));
+        Gson gson = new Gson();
+
+        for (String line : stream) {
+            try {
+                JSONObject o = new JSONObject(line);
+                TableIndex tableIndex = new TableIndex();
+                tableIndex.table = TABLEM.parseFromJSON(o);
+                if (tableIndex.table == null) {
+                    continue;
+                }
+                tableIndex.caption = o.has("title") ? o.getString("title") : "";
+                tableIndex.pageTitle = o.has("pageTitle") ? o.getString("pageTitle") : "";
+                tableIndex.pageContent = o.has("plainTextContent") ? o.getString("plainTextContent") : "";
+                tableIndex.tableText = o.has("table_as_text") ? o.getString("table_as_text") : "";
+
+                out.println(gson.toJson(tableIndex));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        out.close();
     }
 
+    public static void main(String[] args) throws Exception {
+        processTableData(args);
+//        System.out.println(deleteIndex());
+//        System.out.println(createIndex());
+//        System.out.println("Importing tables for TABLEM:");
+//        System.out.println(importTables("/GW/D5data-11/hvthinh/TABLEM/all/all+id.shuf.gz"));
+    }
 }
