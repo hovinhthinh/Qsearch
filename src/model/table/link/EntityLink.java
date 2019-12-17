38c4e7c966c508d729023a30057c6ff4d07eae03
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-12-17 23:52:19 +0100

    update

--- src/model/table/link/EntityLink.java
+++ src/model/table/link/EntityLink.java
@@ -5,7 +5,7 @@ import util.Pair;
 import java.util.List;
 
 public class EntityLink {
-    public List<Pair<String, Integer>> candidates; // candidate with freq: # e.g. Cristiano_Ronaldo (with < and > )
+    public List<Pair<String, Integer>> candidates; // all candidates with freq: # e.g. Cristiano_Ronaldo (with < and > )
     public String target; // target # e.g., YAGO:Cristiano_Ronaldo (without '<' and '>')
     public String text; // anchor text
 }
