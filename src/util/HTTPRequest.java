75487dd20f4c198eaa752ead64506aaa30d05249
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-12-22 23:41:23 +0100

    update

--- src/util/HTTPRequest.java
+++ src/util/HTTPRequest.java
@@ -6,15 +6,15 @@ public class HTTPRequest {
     }
 
     public static String POST(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "POST", body);
+        return Crawler.getContentFromUrl(url, null, "POST", body, null);
     }
 
     public static String PUT(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "PUT", body);
+        return Crawler.getContentFromUrl(url, null, "PUT", body, null);
     }
 
     public static String DELETE(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "DELETE", body);
+        return Crawler.getContentFromUrl(url, null, "DELETE", body, null);
     }
 
 }
