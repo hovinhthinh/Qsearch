49efffb3969c9fda3327e891627b766ea9819661
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-12-26 20:57:32 +0100

    update

--- src/tmp/Tmp.java
+++ src/tmp/Tmp.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -24,44 +23,33 @@ public class Tmp {
         private Text word = new Text();
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String line = value.toString();
+            String line = value.toString() + " err";
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
                 word.set(tokenizer.nextToken());
-                context.write(word, one);
+                context.write(word, null);
             }
-        }
-    }
-
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
-                throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            context.write(key, new IntWritable(sum));
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
 
-        Job job = new Job(conf, "wordcount");
+        Job job = new Job(conf);
 
         job.setJarByClass(Tmp.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
         job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
+        job.setNumReduceTasks(0);
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path("/user/hvthinh/test.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("/user/hvthinh/test2.txt"));
+        FileInputFormat.setInputDirRecursive(job, true);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path("/user/hvthinh/test.txt"));
 
         job.waitForCompletion(true);
     }
