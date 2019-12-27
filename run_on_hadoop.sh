3fe1d49418045d22eada9b2164b5f78721880f1b
hvthinh <hvthinh@mpi-inf.mpg.de>
2019-12-27 01:36:57 +0100

    up

--- run_on_hadoop.sh
+++ run_on_hadoop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # args: <mapperClass>, <inputOnHDFS>, <outputOnHDFS>
-mvn clean install
+mvn clean package
 hadoop jar target/tabqs-1.0-SNAPSHOT-job.jar util.hadoop.MapOnlyJob -Dmapreduce.map.memory.mb=8192 $1 $2 $3
