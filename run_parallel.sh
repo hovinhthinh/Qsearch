4445dd020bb9ffaf07e1bbcbf4d083df085a1a70
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-01-03 18:05:05 +0100

    update

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Use to run a task to process a input file with multithreading. Each element of the input file and output file should be in a single line.
-# <input> and <output> of the MainClass should be the first 2 arguments. MainClass processes input and output files in BZ2 format.
+# <input> and <output> of the MainClass should be the first 2 arguments. MainClass processes input and output files in GZIP format.
 # run_parallel.sh <nProcesses> <MainClass> <input> <output> <more args...>
-# Real output file will be also in BZ2 format.
+# Real output file will be also in GZIP format.
 
 textifyDuration() {
    local duration=$1
@@ -33,20 +33,20 @@ args="$3 $1"
 mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="util.FileSplitter" -Dexec.args="$args"
 
 echo "Run in parallel"
-for i in $(seq -f "%02g" 0 ${np}); do
-args="$3.part$i.bz2 $4.part$i.bz2 ${@:5}"
+for i in $(seq -f "%03g" 0 ${np}); do
+args="$3.part$i.gz $4.part$i.gz ${@:5}"
 export MAVEN_OPTS="-Xmx12G" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args" 1>$4.part$i.out 2>$4.part$i.err &
 done
 
 wait
 
 echo "Combine result"
-bzcat $4.part*.bz2 | bzip2 > $4
+zcat $4.part*.gz | gzip > $4
 echo "Clean"
 # Remove input parts.
-rm $3.part*.bz2
+rm $3.part*.gz
 # Remove output parts.
-rm $4.part*.bz2
+rm $4.part*.gz
 
 # END JOB
 
--- src/util/FileSplitter.java
+++ src/util/FileSplitter.java
@@ -10,8 +10,8 @@ import java.util.Collections;
 
 public class FileSplitter {
     // Args: <input> <nParts>
-    // nPart should be <= 100
-    // Output files: <input>.part00.bz2, <input>.part01.bz2,..., <input>.part<nPart>.bz2; with BZ2 compression.
+    // nPart should be <= 1000
+    // Output files: <input>.part000.gz, <input>.part001.gz,..., <input>.part<nPart>.gz; with GZIP compression.
     //
     // or:
     // <input> <output1> <ratio1> <output2> <ratio2> ... <outputn>
@@ -21,7 +21,7 @@ public class FileSplitter {
         if (args.length == 2) {
             PrintWriter[] outs = new PrintWriter[Integer.parseInt(args[1])];
             for (int i = 0; i < outs.length; ++i) {
-                outs[i] = FileUtils.getPrintWriter(String.format("%s.part%02d.bz2", args[0], i), "UTF-8");
+                outs[i] = FileUtils.getPrintWriter(String.format("%s.part%03d.gz", args[0], i), "UTF-8");
             }
 
             if (args[0].endsWith(".tar.bz2")) { // Each entry is a file now.
