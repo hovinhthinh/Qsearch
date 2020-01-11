0e6d9bb767c8c72221b1cae63d0fbdaf6de7ebf9
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-01-11 14:32:03 +0100

    update

--- src/eval/equity/EvaluateEquity.java
+++ src/eval/equity/EvaluateEquity.java
@@ -2,13 +2,21 @@ package eval.equity;
 
 import com.google.gson.Gson;
 import eval.TruthTable;
-import pipeline.*;
+import pipeline.ColumnLinkFilteringNode;
+import pipeline.DeepColumnScoringNode;
+import pipeline.PostFilteringNode;
+import pipeline.TaggingPipeline;
+import pipeline.deep.DeepScoringClient;
+import util.Constants;
 import util.FileUtils;
 
 public class EvaluateEquity {
     public static TaggingPipeline getPipeline() {
         return new TaggingPipeline(
-                new DeepColumnScoringNode(DeepColumnScoringNode.JOINT_INFERENCE),
+                new DeepColumnScoringNode(
+                        DeepColumnScoringNode.JOINT_INFERENCE,
+                        new DeepScoringClient(true, -1),
+                        Constants.MAX_DOUBLE),
                 new ColumnLinkFilteringNode(0),
                 new PostFilteringNode()
         );
@@ -17,12 +25,28 @@ public class EvaluateEquity {
     public static void main(String[] args) {
         TaggingPipeline pipeline = getPipeline();
         Gson gson = new Gson();
+        double microAvg = 0;
+        int nGoodTable = 0;
+        int nBadTable = 0;
         for (String line : FileUtils.getLineStream("eval/equity/dataset/AnnotatedTables-19092016/dataset_processed.json", "UTF-8")) {
             TruthTable table = gson.fromJson(line, TruthTable.class);
+            System.out.println(table.getTableContentPrintable(false, true, false));
+            System.out.println(table.getTableContentPrintable(true, true, false));
             pipeline.tag(table);
-        }
 
-        // TODO: write evaluate code
+            double prec = table.getPrecisionFromTarget();
+            if (prec == -1) {
+                ++nBadTable;
+                continue;
+            }
+            ++nGoodTable;
+            microAvg += prec;
+        }
+        System.out.println("nBadTable: " + nBadTable);
+        System.out.println("nGoodTable: " + nGoodTable);
+        System.out.println(String.format("Micro-Avg-Prec: %.2f", microAvg / nGoodTable * 100));
+
+        // TODO: write evaluate code for getPrecisionFromTarget.
 
         System.exit(0);
     }
