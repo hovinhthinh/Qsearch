f01fade32226a8c60df731508e94ad26e6bc7696
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-01-11 14:12:11 +0100

    update

--- src/eval/TruthTable.java
+++ src/eval/TruthTable.java
@@ -3,6 +3,7 @@ package eval;
 
 import com.google.gson.Gson;
 import model.table.Table;
+import model.table.link.EntityLink;
 
 import java.util.Arrays;
 
@@ -25,5 +26,49 @@ public class TruthTable extends Table {
 
         truth.bodyEntityTarget = new String[truth.nDataRow][truth.nColumn];
         return truth;
+    }
+
+    public double getPrecisionFromFirstCandidate() {
+        int total = 0;
+        int nTrue = 0;
+        for (int i = 0; i < bodyEntityTarget.length; ++i) {
+            for (int j = 0; j < bodyEntityTarget[i].length; ++j) {
+                EntityLink el = data[i][j].getRepresentativeEntityLink();
+                if (el == null || el.candidates == null) {
+                    continue;
+                }
+                ++total;
+
+                String target = el.candidates.get(0).first;
+                if (bodyEntityTarget[i][j].equals(target)) {
+                    ++nTrue;
+                }
+            }
+        }
+        if (total == 0) {
+            return -1;
+        }
+        return ((double) nTrue) / total;
+    }
+
+    public double getPrecisionFromTarget() {
+        int total = 0;
+        int nTrue = 0;
+        for (int i = 0; i < bodyEntityTarget.length; ++i) {
+            for (int j = 0; j < bodyEntityTarget[i].length; ++j) {
+                EntityLink el = data[i][j].getRepresentativeEntityLink();
+                if (el == null) {
+                    continue;
+                }
+                ++total;
+
+                String predictedTarget = data[i][j].getRepresentativeEntityLink().target;
+                predictedTarget = "<" + predictedTarget.substring(predictedTarget.lastIndexOf(":") + 1) + ">";
+                if (predictedTarget.equals(bodyEntityTarget[i][j])) {
+                    ++nTrue;
+                }
+            }
+        }
+        return ((double) nTrue) / total;
     }
 }
