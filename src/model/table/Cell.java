a5650132f767f37edfa16b87e248241a809bb750
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-01-17 15:22:03 +0100

    up

--- src/model/table/Cell.java
+++ src/model/table/Cell.java
@@ -25,9 +25,9 @@ public class Cell {
     private transient String disambiguatedText = null;
 
     public String getDisambiguatedText() {
-        if (disambiguatedText != null) {
-            return disambiguatedText;
-        }
+//        if (disambiguatedText != null) {
+//            return disambiguatedText;
+//        }
         disambiguatedText = this.text;
         for (EntityLink l : entityLinks) {
             disambiguatedText = disambiguatedText.replace(l.text, "<" + l.target.substring(l.target.lastIndexOf(":") + 1) + ">");
