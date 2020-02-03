7ac08f33b981d36efb73708a912ac06a0804d6dc
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-03 13:54:21 +0100

    up

---
+++ src/yago/QfactTaxonomyGraph.java
@@ -0,0 +1,137 @@
+package yago;
+
+import model.quantity.Quantity;
+import util.FileUtils;
+import util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class QfactTaxonomyGraph extends TaxonomyGraph {
+    public static final Logger LOGGER = Logger.getLogger(QfactTaxonomyGraph.class.getName());
+    public static final String DEFAULT_QFACT_FILE = "./non-deep/qfact_text.gz";
+
+    private ArrayList<ArrayList<Pair<String, Quantity>>> entityQfactLists;
+    private ArrayList<ArrayList<Pair<Integer, Integer>>> taxonomyEntityWithQfactLists; // Pair<entityId, distance>
+
+    private void exploreTypeIds(int id, int currentDist, int distLimit, HashMap<Integer, Integer> typeId2Dist) {
+        if (currentDist > distLimit || typeId2Dist.containsKey(id)) {
+            return;
+        }
+        typeId2Dist.put(id, currentDist);
+        for (int v : typeDadLists.get(id)) {
+            exploreTypeIds(v, currentDist + 1, distLimit, typeId2Dist);
+        }
+    }
+
+    public QfactTaxonomyGraph(String qfactFile) {
+        LOGGER.info("Loading YAGO Qfact taxonomy graph");
+        entityQfactLists = new ArrayList<>(nEntities);
+        for (int i = 0; i < nEntities; ++i) {
+            entityQfactLists.add(new ArrayList<>());
+        }
+        for (String line : FileUtils.getLineStream(qfactFile, "UTF-8")) {
+            String[] arr = line.split("\t");
+            Integer entityId = entity2Id.get(arr[0]);
+            if (entityId == null) {
+                continue;
+            }
+            entityQfactLists.get(entityId).add(new Pair<>(arr[1], Quantity.fromQuantityString(arr[2])));
+        }
+
+        LOGGER.info("Populating entities with Qfact to taxonomy.");
+        taxonomyEntityWithQfactLists = new ArrayList<>(nTypes);
+        for (int i = 0; i < nTypes; ++i) {
+            taxonomyEntityWithQfactLists.add(new ArrayList<>());
+        }
+        for (int i = 0; i < nEntities; ++i) {
+            ArrayList<Pair<String, Quantity>> qfacts = entityQfactLists.get(i);
+            if (qfacts.size() == 0) {
+                entityQfactLists.set(i, null);
+                continue;
+            }
+            qfacts.trimToSize();
+
+            // populate for a single entity
+            HashMap<Integer, Integer> typeId2Dist = new HashMap<>();
+            for (int v : entityTypeLists.get(i)) {
+                exploreTypeIds(v, 1, Integer.MAX_VALUE, typeId2Dist);
+            }
+            for (Map.Entry<Integer, Integer> e : typeId2Dist.entrySet()) {
+                taxonomyEntityWithQfactLists.get(e.getKey()).add(new Pair<>(i, e.getValue()));
+            }
+        }
+        for (int i = 0; i < nTypes; ++i) {
+            ArrayList<Pair<Integer, Integer>> entitiesWithQfact = taxonomyEntityWithQfactLists.get(i);
+            if (entitiesWithQfact.size() == 0) {
+                taxonomyEntityWithQfactLists.set(i, null);
+                continue;
+            }
+            // sort by distance
+            Collections.sort(entitiesWithQfact, (a, b) -> a.second.compareTo(b.second));
+        }
+    }
+
+    public QfactTaxonomyGraph() {
+        this(DEFAULT_QFACT_FILE);
+    }
+
+    // returns Pair<entityId, distance>, sorted by distance
+    public ArrayList<Pair<Integer, Integer>> getSimilarEntityIdsWithQfact(String entity, int distanceLimit) {
+        // Go up to get type list.
+        HashMap<Integer, Integer> typeId2Distance = new HashMap<>();
+        Integer entityId = entity2Id.get(entity);
+        if (entityId == null) {
+            return null;
+        }
+        for (int v : entityTypeLists.get(entityId)) {
+            exploreTypeIds(v, 1, distanceLimit - 1, typeId2Distance);
+        }
+        HashMap<Integer, Integer> entityId2Distance = new HashMap<>();
+        for (Map.Entry<Integer, Integer> e : typeId2Distance.entrySet()) {
+            ArrayList<Pair<Integer, Integer>> entitiesWithQfact = taxonomyEntityWithQfactLists.get(e.getKey());
+            if (entitiesWithQfact == null) {
+                continue;
+            }
+            for (Pair<Integer, Integer> p : entitiesWithQfact) {
+                if (p.second > distanceLimit - e.getValue()) {
+                    break;
+                }
+                int currentDist = entityId2Distance.getOrDefault(p.first, Integer.MAX_VALUE);
+                if (currentDist > e.getValue() + p.second) {
+                    entityId2Distance.put(p.first, e.getValue() + p.second);
+                }
+            }
+        }
+        return entityId2Distance.entrySet().stream().sorted((a, b) -> a.getValue().compareTo(b.getValue())).map(o -> {
+            return new Pair<>(o.getKey(), o.getValue());
+        }).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    // returns Pair<entity, distance>, sorted by distance
+    public ArrayList<Pair<String, Integer>> getSimilarEntitiesWithQfact(String entity, int distanceLimit) {
+        return getSimilarEntityIdsWithQfact(entity, distanceLimit).stream().map(o -> {
+            return new Pair<>(id2Entity.get(o.first), o.second);
+        }).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static void main(String[] args) {
+        QfactTaxonomyGraph graph = new QfactTaxonomyGraph();
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Cristiano_Ronaldo>", 2).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Cristiano_Ronaldo>", 3).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Cristiano_Ronaldo>", 4).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Cristiano_Ronaldo>", 5).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Cristiano_Ronaldo>", 10).size());
+
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Barack_Obama>", 2).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Barack_Obama>", 3).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Barack_Obama>", 4).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Barack_Obama>", 5).size());
+        System.out.println(graph.getSimilarEntitiesWithQfact("<Barack_Obama>", 10).size());
+
+    }
+}
---
+++ src/yago/TaxonomyGraph.java
@@ -0,0 +1,110 @@
+package yago;
+
+import util.FileUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+public class TaxonomyGraph {
+    public static final Logger LOGGER = Logger.getLogger(TaxonomyGraph.class.getName());
+    public static final String YAGO_TAXONOMY_FILE = "/GW/D5data-11/hvthinh/yago-taxonomy/yagoTaxonomy.tsv.gz";
+    public static final String YAGO_TYPE_FILE = "/GW/D5data-11/hvthinh/yago-taxonomy/yagoTypes.tsv.gz";
+
+    public HashMap<String, Integer> type2Id;
+    public ArrayList<String> id2Type;
+    public ArrayList<ArrayList<Integer>> typeDadLists;
+
+    public HashMap<String, Integer> entity2Id;
+    public ArrayList<String> id2Entity;
+    public ArrayList<ArrayList<Integer>> entityTypeLists;
+    public int nEntities;
+    public int nTypes;
+
+    public int getTypeId(String type, boolean addIfAbsent) {
+        Integer id = type2Id.get(type);
+        if (id != null) {
+            return id;
+        }
+        if (!addIfAbsent) {
+            return -1;
+        }
+        type2Id.put(type, type2Id.size());
+        id2Type.add(type);
+        typeDadLists.add(new ArrayList<>());
+        return id2Type.size() - 1;
+    }
+
+
+    public int getEntityId(String entity, boolean addIfAbsent) {
+        Integer id = entity2Id.get(entity);
+        if (id != null) {
+            return id;
+        }
+        if (!addIfAbsent) {
+            return -1;
+        }
+        entity2Id.put(entity, entity2Id.size());
+        id2Entity.add(entity);
+        entityTypeLists.add(new ArrayList<>());
+        return id2Entity.size() - 1;
+    }
+
+    public TaxonomyGraph(String yagoTaxonomyFile, String yagoTypeFile) {
+        LOGGER.info("Loading YAGO taxonomy graph");
+        // Load taxonomy
+        type2Id = new HashMap<>();
+        id2Type = new ArrayList<>();
+        typeDadLists = new ArrayList<>();
+        for (String line : FileUtils.getLineStream(yagoTaxonomyFile, "UTF-8")) {
+            String[] arr = line.split("\t");
+            if (arr.length != 4 || !arr[2].equals("rdfs:subClassOf")) {
+                continue;
+            }
+            String childType = arr[1], parentType = arr[3];
+            int childId = getTypeId(childType, true), dadId = getTypeId(parentType, true);
+            typeDadLists.get(childId).add(dadId);
+        }
+        id2Type.trimToSize();
+        typeDadLists.trimToSize();
+        for (ArrayList<Integer> l : typeDadLists) {
+            l.trimToSize();
+        }
+        nTypes = id2Type.size();
+
+        // Load entity types
+        entity2Id = new HashMap<>();
+        id2Entity = new ArrayList<>();
+        entityTypeLists = new ArrayList<>();
+        for (String line : FileUtils.getLineStream(yagoTypeFile, "UTF-8")) {
+            String[] arr = line.split("\t");
+            if (arr.length != 4 || !arr[2].equals("rdf:type")) {
+                continue;
+            }
+            String entity = arr[1], type = arr[3];
+//            String type = NLP.fastStemming(NLP.stripSentence(arr[3].replaceAll("[^A-Za-z0-9]", " ")).toLowerCase(), Morpha.noun);
+//            if (type.startsWith("wikicat ")) {
+//                type = type.substring(8);
+//            }
+//            if (type.startsWith("wordnet ")) {
+//                type = type.substring(type.indexOf(" ") + 1, type.lastIndexOf(" "));
+//            }
+
+            int entityId = getEntityId(entity, true), typeId = getTypeId(type, false);
+            if (typeId == -1) {
+                throw new RuntimeException("type not found");
+            }
+            entityTypeLists.get(entityId).add(typeId);
+        }
+        id2Entity.trimToSize();
+        entityTypeLists.trimToSize();
+        for (ArrayList<Integer> l : entityTypeLists) {
+            l.trimToSize();
+        }
+        nEntities = id2Entity.size();
+    }
+
+    public TaxonomyGraph() {
+        this(YAGO_TAXONOMY_FILE, YAGO_TYPE_FILE);
+    }
+}
