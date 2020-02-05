2d90728f430e8c2f880e19f3f7c9a907f5ed3e46
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-05 22:57:02 +0100

    up

--- src/model/context/ContextEmbeddingMatcher.java
+++ src/model/context/ContextEmbeddingMatcher.java
@@ -12,6 +12,7 @@ public class ContextEmbeddingMatcher implements ContextMatcher {
         this.alpha = alpha;
     }
 
+    // Lower is better
     public double directedEmbeddingIdfDistance(ArrayList<String> queryX, ArrayList<String> factX) {
         // TODO: Currently not supporting TIME (TIME is computed like normal terms).
         double score = 0;
@@ -26,6 +27,26 @@ public class ContextEmbeddingMatcher implements ContextMatcher {
             }
             double idf = IDF.getRobertsonIdf(qX);
             score += min * idf;
+            totalIdf += idf;
+        }
+        return score / totalIdf + 1;
+    }
+
+    // Higher is better
+    public double directedEmbeddingIdfSimilarity(ArrayList<String> queryX, ArrayList<String> factX) {
+        // TODO: Currently not supporting TIME (TIME is computed like normal terms).
+        double score = 0;
+        double totalIdf = 0;
+        for (String qX : queryX) {
+            double max = 0;
+            for (String fX : factX) {
+                double sim = 1 - Glove.cosineDistance(qX, fX);
+                if (sim != -1) {
+                    max = Math.max(max, sim);
+                }
+            }
+            double idf = IDF.getRobertsonIdf(qX);
+            score += max * idf;
             totalIdf += idf;
         }
         return score / totalIdf + 1;
