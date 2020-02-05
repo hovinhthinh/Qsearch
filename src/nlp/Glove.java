2340b4844506f6f49cde20068594da5089a640a0
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-05 14:25:22 +0100

    up

--- src/nlp/Glove.java
+++ src/nlp/Glove.java
@@ -1,10 +1,10 @@
 package nlp;
 
-import org.apache.commons.collections4.map.LRUMap;
+import it.unimi.dsi.fastutil.longs.Long2DoubleLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import util.FileUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 // Return a non-negative value. A value of 0 means that the 2 strings are the same.
 // A value of -1 means that similarity cannot be computed (embedding is unavailable).
@@ -12,17 +12,18 @@ public class Glove {
     public static final String DATA_DIR = "./resources/glove/";
     public static final int DIM = 100; // 50, 100, 200 or 300.
 
-    private static HashMap<String, Integer> EMBEDDING_ID = null;
+    private static Object2IntOpenHashMap<String> EMBEDDING_ID = null;
     private static ArrayList<double[]> EMBEDDING_VALUE = null;
 
-    private static LRUMap<Long, Double> CACHE_COSINE = new LRUMap<>(10000000);
+    private static final int CACHE_COSINE_SIZE = 10000000;
+    private static Long2DoubleLinkedOpenHashMap CACHE_COSINE = new Long2DoubleLinkedOpenHashMap(CACHE_COSINE_SIZE);
 
     private synchronized static void init() {
         if (EMBEDDING_ID != null) {
             return;
         }
         String embeddingFile = DATA_DIR + "glove.6B." + DIM + "d.txt";
-        HashMap<String, Integer> tempMap = new HashMap<>();
+        Object2IntOpenHashMap<String> tempMap = new Object2IntOpenHashMap<>();
         ArrayList<double[]> tempValue = new ArrayList<>();
         for (String line : FileUtils.getLineStream(embeddingFile, "UTF-8")) {
             String[] arr = line.split(" ");
@@ -34,7 +35,12 @@ public class Glove {
             tempValue.add(embedding);
         }
         EMBEDDING_ID = tempMap;
+        EMBEDDING_ID.defaultReturnValue(-1);
+
         EMBEDDING_VALUE = tempValue;
+        EMBEDDING_VALUE.trimToSize();
+
+        CACHE_COSINE.defaultReturnValue(-1.0);
     }
 
     // Normally cosine gives a value from -1 to 1. However, we normalize this value to 0 -> 1
@@ -47,24 +53,20 @@ public class Glove {
             return 0;
         }
 
-        Integer aId = EMBEDDING_ID.get(a);
-        if (aId == null) {
-            return -1;
-        }
-        Integer bId = EMBEDDING_ID.get(b);
-        if (bId == null) {
+        int aId, bId;
+        if ((aId = EMBEDDING_ID.getInt(a)) == -1 || (bId = EMBEDDING_ID.getInt(b)) == -1) {
             return -1;
         }
 
         if (aId > bId) {
-            Integer tmp = aId;
+            int tmp = aId;
             aId = bId;
             bId = tmp;
         }
 
         long key = ((long) aId) * EMBEDDING_VALUE.size() + bId;
-        Double r = CACHE_COSINE.get(key);
-        if (r != null) {
+        double r = CACHE_COSINE.getAndMoveToFirst(key);
+        if (r != -1.0) {
             return r;
         }
 
@@ -82,7 +84,10 @@ public class Glove {
         // Normalize.
         double cosine = 0.5 - dotProduct / Math.sqrt(aLength) / Math.sqrt(bLength) / 2;
 
-        CACHE_COSINE.put(key, cosine);
+        CACHE_COSINE.putAndMoveToFirst(key, cosine);
+        if (CACHE_COSINE.size() > CACHE_COSINE_SIZE) {
+            CACHE_COSINE.removeLastDouble();
+        }
         return cosine;
     }
 
