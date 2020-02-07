bb739bd7107945f3c2d74ec7e1534ef2bf5c9e82
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-07 14:24:15 +0100

    update

--- src/model/context/IDF.java
+++ src/model/context/IDF.java
@@ -14,6 +14,8 @@ public class IDF {
     private static Map<String, Double> ROBERTSON_IDF;
     private static double N_DOC;
     private static double MIN_IDF = 0.0001;
+    private static double OOV_DEFAULT_IDF;
+    private static double OOV_ROBERTSON_IDF;
 
     static {
         DEFAULT_IDF = new HashMap<>();
@@ -29,6 +31,8 @@ public class IDF {
                 ROBERTSON_IDF.put(arr[0], Math.max(MIN_IDF, Math.log10((N_DOC - df + 0.5) / (df + 0.5))));
             }
         }
+        OOV_DEFAULT_IDF = Math.max(MIN_IDF, Math.log(N_DOC / (0 + 1))); // df = 0
+        OOV_ROBERTSON_IDF = Math.max(MIN_IDF, Math.log10((N_DOC - 0 + 0.5) / (0 + 0.5))); // df = 0
     }
 
 
@@ -43,7 +47,7 @@ public class IDF {
         if (idf == 0 && !allowOOV) {
             return MIN_IDF;
         }
-        return idf;
+        return idf != 0 ? idf : OOV_DEFAULT_IDF;
     }
 
 
@@ -58,7 +62,7 @@ public class IDF {
         if (idf == 0 && !allowOOV) {
             return MIN_IDF;
         }
-        return idf;
+        return idf != 0 ? idf : OOV_ROBERTSON_IDF;
     }
 
 }
