8a8f705bf408a3800a958a6eafac8dc6faef9d58
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-18 12:57:39 +0100

    update

---
+++ src/eval/wiki_random/ProcessWikiRandom.java
@@ -0,0 +1,73 @@
+package eval.wiki_random;
+
+import com.google.gson.Gson;
+import data.background.mention2entity.Mention2EntityPrior;
+import eval.TruthTable;
+import model.table.link.EntityLink;
+import pipeline.*;
+import util.FileUtils;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ProcessWikiRandom {
+    // Just the annotations of entities and quantities, there is no linking.
+    public static TaggingPipeline getPipeline() {
+        return new TaggingPipeline(
+                new TablePrefilteringNode(),
+                new TimeTaggingNode(),
+                new QuantityTaggingNode(),
+                new ColumnTypeTaggingNode(0.3, 0.3)
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+        TaggingPipeline pipeline = getPipeline();
+
+        Scanner in = new Scanner(System.in);
+
+        Mention2EntityPrior mention2EntityPrior = new Mention2EntityPrior(1, 10);
+
+        PrintWriter out = FileUtils.getPrintWriter("eval/wiki_random/wiki_random_annotation.gz", "UTF-8");
+        int nGood = 0;
+        Gson gson = new Gson();
+        for (String line : FileUtils.getLineStream("eval/wiki_random/wiki_random_ground.gz", "UTF-8")) {
+
+            TruthTable t;
+            t = gson.fromJson(line, TruthTable.class);
+
+            for (int i = 0; i < t.nDataRow; ++i) {
+                for (int j = 0; j < t.nColumn; ++j) {
+                    EntityLink el = t.data[i][j].getRepresentativeEntityLink();
+                    if (el != null) {
+                        el.candidates = mention2EntityPrior.getCanditateEntitiesForMention(el.text);
+                        if (el.candidates == null) {
+                            t.data[i][j].entityLinks.clear();
+                            t.bodyEntityTarget[i][j] = null;
+                            t.data[i][j].resetCachedRepresentativeLink();
+                        }
+                    }
+                }
+            }
+
+            if (!pipeline.tag(t)) {
+                System.out.println("Ignored: " + t._id);
+                continue;
+            }
+            ++nGood;
+
+            out.println(gson.toJson(t));
+//            System.out.println(line);
+//            System.out.println("source: " + t.source);
+//            System.out.println("caption: " + t.caption);
+//            System.out.println(t.getTableContentPrintable(false, true, false));
+//            System.out.println("Annotated:");
+//            System.out.println(t.getTableContentPrintable(true, true, true));
+//            System.out.println("--------------------------------------------------------------------------------");
+//            System.out.flush();
+//            String wait = in.nextLine();
+        }
+        System.out.println("nGood Tables: " + nGood);
+        out.close();
+    }
+}
