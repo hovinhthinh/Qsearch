f4e2d178e9ae1725ea964be48de87644f99ff38f
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-18 15:39:36 +0100

    git c

---
+++ src/eval/wiki_random/AnnotateTableLinking.java
@@ -0,0 +1,113 @@
+package eval.wiki_random;
+
+import com.google.gson.Gson;
+import eval.TruthTable;
+import util.FileUtils;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AnnotateTableLinking {
+    static int nIgnored, nAnnotated;
+
+    // args: <input> <output> [ignore first n tables]
+    public static void main(String[] args) throws Exception {
+        args = "eval/wiki_random/wiki_random_annotation.gz eval/wiki_random/wiki_random_annotation_linking.gz 0".split(" ");
+
+        PrintWriter out = FileUtils.getPrintWriter(args[1]);
+        Scanner in = new Scanner(System.in);
+        Gson gson = new Gson();
+        int toBeIgnored = args.length == 3 ? Integer.parseInt(args[2]) : 0;
+        nIgnored = toBeIgnored;
+        if (toBeIgnored > 0) {
+            System.out.println("=== Ignoring " + toBeIgnored + " tables ===");
+        }
+        for (String line : FileUtils.getLineStream(args[0], "UTF-8")) {
+            TruthTable t = gson.fromJson(line, TruthTable.class);
+
+            if (toBeIgnored > 0) {
+                --toBeIgnored;
+                continue;
+            }
+            if (t.quantityToEntityColumn == null) {
+                t.quantityToEntityColumn = new int[t.nColumn];
+                Arrays.fill(t.quantityToEntityColumn, -1);
+                t.quantityToEntityColumnScore = new double[t.nColumn];
+                Arrays.fill(t.quantityToEntityColumnScore, -1.0);
+            }
+            boolean invalidOption = false, invalidColumnIndex = false;
+            loop:
+            do {
+                if (!invalidOption && !invalidColumnIndex) {
+                    System.out.println("--------------------------------------------------------------------------------");
+                    System.out.println("source: " + t.source);
+                    System.out.println("pageTitle: " + t.pageTitle);
+                    System.out.println("caption: " + t.caption);
+                    System.out.println("=== Original ===");
+                    System.out.println(t.getTableContentPrintable(false, true, false, true));
+                    System.out.println("=== Annotated ===");
+                    System.out.println(t.getTableContentPrintable(true, true, true, true));
+                    System.out.println("----------");
+                    System.out.println("annotated/total: " + nAnnotated + " / " + (nAnnotated + nIgnored));
+                    System.out.println("=== OPTIONS ===");
+                    System.out.println("          [q1,q2,.. > e] or [all > e]: set annotation: link[q] = e");
+                    System.out.println("          [i]: ignore table");
+                    System.out.println("          [s]: save table annotation");
+                }
+                if (invalidOption) {
+                    System.out.println("INVALID OPTION!");
+                    invalidOption = false;
+                }
+                if (invalidColumnIndex) {
+                    System.out.println("INVALID COLUMN INDEX!");
+                    invalidOption = false;
+                }
+                System.out.print(">>> ");
+                String cmd = in.nextLine();
+                // process command.
+                cmd = cmd.replaceAll("\\s", "");
+                if (cmd.equals("i")) {
+                    ++nIgnored;
+                    break;
+                } else if (cmd.equals("s")) {
+                    t.quantityToEntityColumnGroundTruth = Arrays.copyOf(t.quantityToEntityColumn, t.nColumn);
+                    out.println(gson.toJson(t));
+                    out.flush();
+                    ++nAnnotated;
+                    break;
+                }
+                try {
+                    int p = cmd.indexOf(">");
+                    int eCol = Integer.parseInt(cmd.substring(p + 1));
+                    String qColsStr = cmd.substring(0, p);
+                    if (qColsStr.equals("all")) {
+                        for (int i = 0; i < t.nColumn; ++i) {
+                            if (t.isNumericColumn[i]) {
+                                t.quantityToEntityColumn[i] = eCol;
+                            }
+                        }
+                        continue;
+                    }
+                    List<Integer> qCols = Arrays.asList(qColsStr.split(",")).stream().map(o -> Integer.parseInt(o)).collect(Collectors.toList());
+                    for (int qCol : qCols) {
+                        if (qCol < 0 || qCol >= t.nColumn || eCol < -1 || eCol >= t.nColumn) {
+                            invalidColumnIndex = true;
+                            continue loop;
+                        }
+                    }
+                    for (int qCol : qCols) {
+                        t.quantityToEntityColumn[qCol] = eCol;
+                    }
+                    continue;
+                } catch (Exception e) {
+                    invalidOption = true;
+                    continue;
+                }
+            } while (true);
+        }
+        out.close();
+    }
+}
---
+++ src/eval/wiki_random/EvaluateWikiRandom.java
@@ -0,0 +1,105 @@
+package eval.wiki_random;
+
+import com.google.gson.Gson;
+import eval.TruthTable;
+import pipeline.ColumnLinkFilteringNode;
+import pipeline.PostFilteringNode;
+import pipeline.TaggingPipeline;
+import pipeline.TextBasedColumnScoringNode;
+import util.FileUtils;
+import util.MetricReporter;
+import util.Pair;
+
+public class EvaluateWikiRandom {
+    public static TaggingPipeline getPipeline() {
+        return new TaggingPipeline(
+                new TextBasedColumnScoringNode(
+                        TextBasedColumnScoringNode.JOINT_INFERENCE,
+                        1
+                ),
+                new ColumnLinkFilteringNode(0),
+                new PostFilteringNode()
+        );
+    }
+
+    public static void main(String[] args) {
+        TaggingPipeline pipeline = getPipeline();
+        Gson gson = new Gson();
+        int nGoodTable = 0, nBadTable = 0;
+
+        MetricReporter reporter = new MetricReporter(EvaluateWikiRandom.class.getName());
+
+        for (String line : FileUtils.getLineStream("eval/wiki_random/wiki_random_annotation.gz", "UTF-8")) {
+            TruthTable table = gson.fromJson(line, TruthTable.class);
+//            table.linkQuantitiesInTableAndText();
+//            double qtFoundRate = table.getRateOfTableQuantitiesFoundInText();
+//            reporter.recordAverage("rateQtFoundRateInText", qtFoundRate);
+//            reporter.recordAverage("macroPrecCAHeaderEmbedding", table.getAlignmentPrecisionFromHeaderEmbedding());
+//            reporter.recordAverage("macroPrecCAColumnEmbedding", table.getAlignmentPrecisionFromColumnEmbedding());
+//            reporter.recordAverage("macroPrecCAColumnJaccardIndex", table.getAlignmentPrecisionFromColumnJaccardIndex());
+
+            System.out.println("--- Original ---");
+            System.out.println("- URL: " + table.source);
+            System.out.println("- Title: " + table.pageTitle);
+//            System.out.println("- Content: \r\n" + table.surroundingTextAsParagraph.toStringWithNewlineAfterSentences());
+            System.out.println("- Caption: " + table.caption);
+            System.out.println(table.getTableContentPrintable(false, true, false, false));
+            System.out.println("--- Ground Truth ---");
+            System.out.println(table.getTableContentPrintable(true, true, true, true));
+
+            double precEDPrior = table.getEntityDisambiguationPrecisionFromPrior();
+            Pair<Integer, Integer> precEDPriorInfo = table.getEntityDisambiguationMicroPrecisionInfoFromPrior();
+            System.out.println("--- Prior ---");
+            System.out.println(table.getTableContentPrintable(true, true, true, true));
+            double precCAFirstColumn = table.getAlignmentPrecisionFromFirstColumn();
+
+            pipeline.tag(table);
+
+            double precEDOurs = table.getEntityDisambiguationPrecisionFromTarget();
+            Pair<Integer, Integer> precEDOursInfo = table.getEntityDisambiguationMicroPrecisionInfoFromTarget();
+
+//            double precCAOurs = table.getAlignmentPrecisionFromTarget();
+
+//            if (precEDOurs == -1 || precEDPrior == -1 || precCAOurs == -1 || precCAFirstColumn == -1 || qtFoundRate == -1) {
+//                ++nBadTable;
+//                continue;
+//            }
+
+            System.out.println("--- Ours ---");
+            System.out.println(table.getTableContentPrintable(true, true, true, true));
+
+//            System.out.println("Linked Qfacts:");
+//            for (int i = 0; i < table.nColumn; ++i) {
+//                if (table.isNumericColumn[i]) {
+//                    System.out.println("+ Column " + i + " --> " + table.quantityToEntityColumn[i] + ":");
+//                    for (int r = 0; r < table.nDataRow; ++r) {
+//                        if (table.QfactMatchingStr[r][i] != null) {
+//                            String e = table.data[r][table.quantityToEntityColumn[i]].getRepresentativeEntityLink().target;
+//                            e = "<" + e.substring(e.lastIndexOf(":") + 1) + ">";
+//                            System.out.println("   " + e + " --> " + table.QfactMatchingStr[r][i]);
+//                        } else {
+//                            System.out.println("   null");
+//                        }
+//                    }
+//                }
+//            }
+            System.out.println(String.format("precEntityDisambiguation: Prior/Ours: %.2f/%.2f", precEDPrior * 100, precEDOurs * 100));
+//            System.out.println(String.format("precColumnAlignment: FirstColumn/Ours: %.2f/%.2f", precCAFirstColumn * 100, precCAOurs * 100));
+//            System.out.println(String.format("qtFoundRateInText: %.2f", qtFoundRate));
+            System.out.println("========================================================================================================================================================================================================");
+            ++nGoodTable;
+
+            reporter.recordAverage("macroPrecEDPrior", precEDPrior);
+            reporter.recordAverage("macroPrecEDOurs", precEDOurs);
+            reporter.recordAverage("macroPrecCAFirstColumn", precCAFirstColumn);
+//            reporter.recordAverage("macroPrecCAOurs", precCAOurs);
+
+            reporter.recordMicroAverage("microPrecEDPrior", precEDPriorInfo);
+            reporter.recordMicroAverage("microPrecEDOurs", precEDOursInfo);
+        }
+        System.out.println("nBadTable/nGoodTable: " + nBadTable + "/" + nGoodTable);
+        System.out.println(reporter.getReportString());
+
+        System.exit(0);
+    }
+}
