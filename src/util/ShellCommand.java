87eed58d9c380fbe3fa5c8aa747e06bfcabb7d9f
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-02-26 18:18:42 +0100

    git c

--- src/util/ShellCommand.java
+++ src/util/ShellCommand.java
@@ -1,7 +1,9 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ShellCommand {
     public static String execute(String command) {
@@ -10,14 +12,27 @@ public class ShellCommand {
         try {
             String[] commands = new String[]{"/bin/sh", "-c", command};
             p = Runtime.getRuntime().exec(commands);
-            p.waitFor();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream(), StandardCharsets.UTF_8));
+            new Thread(() -> {
+                try {
+                    String line;
+                    while ((line = err.readLine()) != null) {
+                        // ignore err
+//                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream(), StandardCharsets.UTF_8));
             String line;
             while ((line = reader.readLine()) != null) {
                 output.append(line + "\r\n");
             }
-        } catch (
-                Exception e) {
+            p.waitFor();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return output.toString();
