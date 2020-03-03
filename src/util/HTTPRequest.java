a868ae2526ee1f2f8bf6944c7e0be7b645146704
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-03-04 00:23:32 +0100

    git c

--- src/util/HTTPRequest.java
+++ src/util/HTTPRequest.java
@@ -2,19 +2,34 @@ package util;
 
 public class HTTPRequest {
     public static String GET(String url) {
-        return Crawler.getContentFromUrl(url);
+        return GET(url, false);
     }
 
     public static String POST(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "POST", body, null);
+        return POST(url, body, false);
     }
 
     public static String PUT(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "PUT", body, null);
+        return PUT(url, body, false);
     }
 
     public static String DELETE(String url, String body) {
-        return Crawler.getContentFromUrl(url, null, "DELETE", body, null);
+        return DELETE(url, body, false);
     }
 
+    public static String GET(String url, boolean forceOnBadResponseCode) {
+        return Crawler.getContentFromUrl(url, null, "GET", null, null, forceOnBadResponseCode);
+    }
+
+    public static String POST(String url, String body, boolean forceOnBadResponseCode) {
+        return Crawler.getContentFromUrl(url, null, "POST", body, null, forceOnBadResponseCode);
+    }
+
+    public static String PUT(String url, String body, boolean forceOnBadResponseCode) {
+        return Crawler.getContentFromUrl(url, null, "PUT", body, null, forceOnBadResponseCode);
+    }
+
+    public static String DELETE(String url, String body, boolean forceOnBadResponseCode) {
+        return Crawler.getContentFromUrl(url, null, "DELETE", body, null, forceOnBadResponseCode);
+    }
 }
