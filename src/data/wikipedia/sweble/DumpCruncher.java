d09f4d747c084451092f266c1705500c905bf95a
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-03-31 12:40:00 +0200

    update
    Former-commit-id: ea00945b6bb97789ab11a33ea11fcd4573b928d2

---
+++ src/data/wikipedia/sweble/DumpCruncher.java
@@ -0,0 +1,307 @@
+/**
+ * Copyright 2011 The Open Source Research Group,
+ * University of Erlangen-Nürnberg
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package data.wikipedia.sweble;
+
+import de.fau.cs.osr.utils.WrappedException;
+import de.fau.cs.osr.utils.getopt.Options;
+import joptsimple.OptionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sweble.wikitext.articlecruncher.*;
+import org.sweble.wikitext.articlecruncher.pnodes.LocalProcessingNode;
+import org.sweble.wikitext.articlecruncher.pnodes.LpnJobProcessorFactory;
+import org.sweble.wikitext.articlecruncher.storers.DummyStorer;
+import org.sweble.wikitext.articlecruncher.utils.AbortHandler;
+import org.sweble.wikitext.articlecruncher.utils.WorkerBase;
+import org.sweble.wikitext.engine.config.WikiConfig;
+import org.sweble.wikitext.engine.utils.DefaultConfigEnWp;
+import util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+
+public class DumpCruncher {
+    private static final Logger logger = LoggerFactory.getLogger(DumpCruncher.class);
+
+    private final Options options = new Options();
+
+    private Nexus nexus;
+
+    private data.wikipedia.sweble.Gui gui;
+
+    private WikiConfig wikiConfig;
+
+    // =========================================================================
+
+    public static void main(String[] args) throws Throwable {
+        new DumpCruncher().run(args);
+    }
+
+    // =========================================================================
+
+    private void run(String[] args) throws Throwable {
+        args = "--processing-workers 8 -d /local/home/hvthinh/datasets/wikipedia_dump/enwiki-20200301-pages-articles-multistream.xml.bz2 ".split(" ");
+        if (!options(args))
+            return;
+
+        logger.info("Starting dump cruncher");
+        try {
+            setUp();
+        } finally {
+            Set<JobTrace> jobTraces = nexus.getJobTraces();
+            for (JobTrace trace : jobTraces)
+                logger.warn("Unfinished job: " + trace.toString());
+
+            logger.info("Number of unfinished jobs: " + jobTraces.size());
+            logger.info("Dump cruncher exiting");
+        }
+    }
+
+    public void setUp() throws Throwable {
+        gui = new data.wikipedia.sweble.Gui(this);
+
+        nexus = new Nexus();
+
+        final File dumpFile = new File(options.value("dump"));
+        final PrintWriter out = FileUtils.getPrintWriter(options.value("dump") + ".out.gz", "UTF-8");
+
+        nexus.setUp(
+                options.value("Nexus.InTrayCapacity", int.class),
+                options.value("Nexus.ProcessedJobsCapacity", int.class),
+                options.value("Nexus.OutTrayCapacity", int.class));
+
+        nexus.addJobGenerator(new JobGeneratorFactory() {
+            @Override
+            public WorkerBase create(
+                    AbortHandler abortHandler,
+                    BlockingQueue<Job> inTray,
+                    JobTraceSet jobTraces) {
+                try {
+                    return new DumpReaderJobGenerator(
+                            DumpCruncher.this,
+                            dumpFile,
+                            // Are dumps always UTF8?
+                            Charset.forName("UTF8"),
+                            abortHandler,
+                            inTray,
+                            jobTraces);
+                } catch (Exception e) {
+                    throw new WrappedException(e);
+                }
+            }
+        });
+
+		/* This is a dummy processor. It can be used in addProcessingNode()
+		 * to just forward the jobs unaltered to the storer. This is useful if
+		 * no processing should take place. You can, for example, simply read
+		 * a dump and directly write it to a database, without further 
+		 * processing.
+		 * 
+		final LpnJobProcessorFactory lpnJPFactory = new LpnJobProcessorFactory()
+		{
+			@Override
+			public Processor createProcessor()
+			{
+				return new Processor()
+				{
+					@Override
+					public CompletedJob process(Job job)
+					{
+						return null;
+					}
+				};
+			}
+		};
+		 */
+
+        wikiConfig = DefaultConfigEnWp.generate();
+
+        final LpnJobProcessorFactory lpnJPFactory = new LpnJobProcessorFactory() {
+            @Override
+            public Processor createProcessor() {
+                return new RevisionProcessor(DumpCruncher.this, out);
+            }
+
+            @Override
+            public String getProcessorNameTemplate() {
+                return "Processor-%02d";
+            }
+        };
+
+        nexus.addProcessingNode(new ProcessingNodeFactory() {
+            @Override
+            public WorkerBase create(
+                    AbortHandler abortHandler,
+                    BlockingQueue<Job> inTray,
+                    BlockingQueue<Job> completedJobs) {
+                final int numWorkers = options.value("Nexus.NumProcessingWorkers", int.class);
+
+                return new LocalProcessingNode(
+                        abortHandler,
+                        inTray,
+                        completedJobs,
+                        lpnJPFactory,
+                        numWorkers);
+            }
+        });
+
+        /* If it makes sense, one could also use multiple storers
+         * to distribute the load.
+         *
+         * Example: If you want to write the results to a database, multiple
+         * storeres can make sense since a single connection might be dominated
+         * by communication overhead between this application and the database
+         * server.
+         */
+        nexus.addStorer(new StorerFactory() {
+            @Override
+            public WorkerBase create(
+                    AbortHandler abortHandler,
+                    JobTraceSet jobTraces,
+                    BlockingQueue<Job> outTray) {
+                try {
+                    return new DummyStorer(abortHandler, jobTraces, outTray);
+                } catch (Exception e) {
+                    throw new WrappedException(e);
+                }
+            }
+        });
+
+        nexus.start();
+
+        gui.close();
+        out.close();
+    }
+
+    // =========================================================================
+
+    public Options getOptions() {
+        return options;
+    }
+
+    public Nexus getNexus() {
+        return nexus;
+    }
+
+    public data.wikipedia.sweble.Gui getGui() {
+        return gui;
+    }
+
+    public WikiConfig getWikiConfig() {
+        return wikiConfig;
+    }
+
+    // =========================================================================
+
+    private boolean options(String[] args) throws IOException {
+        options.createOption('h', "help")
+                .withDescription("Print help message.")
+                .create();
+
+        // ---
+
+        options.createOption('d', "dump")
+                .withDescription("The dump file to read.")
+                .withPropertyKey("DumpCruncher.File")
+                .withArgName("FILE")
+                .withRequiredArg()
+                .create();
+
+        // ---
+
+        options.createPropertyOnlyOption("Nexus.InTrayCapacity")
+                .withDescription("The number of elements the `in tray' queue can hold.")
+                .withDefault("8")
+                .withArgName("N")
+                .withRequiredArg()
+                .create();
+
+        options.createPropertyOnlyOption("Nexus.ProcessedJobsCapacity")
+                .withDescription("The number of elements the `in tray' queue can hold.")
+                .withDefault("8")
+                .withArgName("N")
+                .create();
+
+        options.createPropertyOnlyOption("Nexus.OutTrayCapacity")
+                .withDescription("The number of elements the `in tray' queue can hold.")
+                .withDefault("8")
+                .withArgName("N")
+                .create();
+
+        options.createOption("processing-workers")
+                .withDescription("The number of processing workers.")
+                .withPropertyKey("Nexus.NumProcessingWorkers")
+                .withDefault("4")
+                .withArgName("N")
+                .create();
+
+        options.createOption('P', "properties")
+                .withDescription("A properties file to load additional configuration options from.")
+                .withDefault("dump2db.properties")
+                .withArgName("FILE")
+                .withRequiredArg()
+                .create();
+
+        // ---
+
+        try {
+            options.parse(args);
+
+            if (options.has("help")) {
+                printHelpMessage(options);
+                return false;
+            }
+
+            if (options.has("P"))
+                options.load(new File(options.value("P")));
+
+            options.expected("dump");
+
+            options.optional("P");
+            options.optional("Nexus.InTrayCapacity");
+            options.optional("Nexus.ProcessedJobsCapacity");
+            options.optional("Nexus.OutTrayCapacity");
+            options.optional("Nexus.NumProcessingWorkers");
+
+            options.checkForInvalidOptions();
+
+            return true;
+        } catch (OptionException e) {
+            printArgsErrorMessage(e);
+            return false;
+        }
+    }
+
+    private void printArgsErrorMessage(OptionException e) {
+        System.err.println(e.getMessage());
+        System.err.println("Try `--help' for more information.");
+    }
+
+    private void printHelpMessage(Options options) {
+        options.cmdLineHelp(System.out);
+        System.out.println();
+        options.propertiesHelp(System.out);
+        System.out.println();
+        options.fixedOptionsHelp(System.out);
+        System.out.println();
+    }
+}
---
+++ src/data/wikipedia/sweble/DumpReaderJobGenerator.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright 2011 The Open Source Research Group,
+ * University of Erlangen-Nürnberg
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package data.wikipedia.sweble;
+
+import de.fau.cs.osr.utils.WrappedException;
+import org.sweble.wikitext.articlecruncher.Job;
+import org.sweble.wikitext.articlecruncher.JobTrace;
+import org.sweble.wikitext.articlecruncher.JobTraceSet;
+import org.sweble.wikitext.articlecruncher.utils.AbortHandler;
+import org.sweble.wikitext.articlecruncher.utils.WorkerBase;
+import org.sweble.wikitext.dumpreader.DumpReader;
+import org.sweble.wikitext.dumpreader.export_0_10.PageType;
+import org.sweble.wikitext.dumpreader.export_0_10.RevisionType;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.concurrent.BlockingQueue;
+
+public class DumpReaderJobGenerator extends WorkerBase {
+    private final BlockingQueue<Job> inTray;
+
+    private final JobTraceSet jobTraces;
+
+    private final DumpCruncher dumpCruncher;
+
+    private final DumpReader dumpReader;
+
+    private InputStream is;
+
+    // =========================================================================
+
+    public DumpReaderJobGenerator(
+            DumpCruncher dumpCruncher,
+            File dumpFile,
+            Charset charset,
+            AbortHandler abortHandler,
+            BlockingQueue<Job> inTray,
+            JobTraceSet jobTraces) {
+        super(DumpReaderJobGenerator.class.getSimpleName(), abortHandler);
+
+        this.dumpCruncher = dumpCruncher;
+        this.inTray = inTray;
+        this.jobTraces = jobTraces;
+
+        try {
+            is = new FileInputStream(dumpFile);
+            this.dumpReader = new DumpReader(
+                    is,
+                    charset,
+                    dumpFile.getPath(),
+                    getLogger(),
+                    false) {
+                @Override
+                protected void processPage(Object mediaWiki, Object page) {
+                    try {
+                        DumpReaderJobGenerator.this.processPage(mediaWiki, page);
+                    } catch (InterruptedException e) {
+                        throw new WrappedException(e);
+                    } catch (IOException e) {
+                        throw new WrappedException(e);
+                    }
+                }
+            };
+        } catch (Exception e) {
+            after();
+            throw new WrappedException(e);
+        }
+    }
+
+    @Override
+    public void after() {
+        if (is != null) {
+            try {
+                info("Close the input stream");
+                is.close();
+                is = null;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // =========================================================================
+
+    public long getFileSize() {
+        return dumpReader.getFileSize();
+    }
+
+    public long getDecompressedBytesRead() throws IOException {
+        return dumpReader.getDecompressedBytesRead();
+    }
+
+    public long getCompressedBytesRead() throws IOException {
+        return dumpReader.getCompressedBytesRead();
+    }
+
+    public long getParsedCount() {
+        return dumpReader.getParsedCount();
+    }
+
+    // =========================================================================
+
+    @Override
+    protected void work() throws Throwable {
+        dumpReader.unmarshal();
+    }
+
+    protected void processPage(Object mediaWiki, Object page_) throws InterruptedException, IOException {
+        PageType page = (PageType) page_;
+
+        for (Object o : page.getRevisionOrUpload()) {
+            if (o instanceof RevisionType) {
+                data.wikipedia.sweble.RevisionJob job = new RevisionJob(page, (RevisionType) o);
+
+                JobTrace trace = job.getTrace();
+                trace.signOff(getClass(), null);
+
+                jobTraces.add(trace);
+
+                inTray.put(job);
+
+                Gui gui = dumpCruncher.getGui();
+                gui.setPageCount((int) getParsedCount());
+                gui.setBytesRead(getCompressedBytesRead());
+                gui.redrawLater();
+            }
+        }
+    }
+}
