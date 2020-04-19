c0d235ea2370fabc9ed682eb142555a07ce6fc9e
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-04-19 17:57:29 +0200

    update

--- src/util/distributed/TestMap.java
+++ src/util/distributed/TestMap.java
@@ -10,7 +10,7 @@ public class TestMap implements String2StringMap {
     public List<String> map(String input) {
         String o1 = new JSONObject().put("input", input).toString();
         String o2 = new JSONObject().put("input_repeated", input).toString();
-        System.err.println(o1);
+//        System.err.println(o1);
         return Arrays.asList(o1, o2);
     }
 }
