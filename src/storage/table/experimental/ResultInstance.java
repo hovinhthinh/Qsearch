5d3e24848d3718bcd0198eeb9d0c00c34e6594eb
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-04-25 18:08:09 +0200

    update
    Former-commit-id: cf7d55c2caf27da76189ec6bd85ef34404ae2986

---
+++ src/storage/table/experimental/ResultInstance.java
@@ -0,0 +1,34 @@
+package storage.table.experimental;
+
+import java.util.ArrayList;
+
+@Deprecated
+public class ResultInstance {
+    public String entity;
+    public double score;
+
+    public static class SubInstance {
+        double score;
+        String quantity;
+        String context;
+        String domain;
+        String source;
+    }
+
+    public ArrayList<SubInstance> subInstances = new ArrayList<>();
+
+    public void addSubInstance(SubInstance si) {
+        subInstances.add(si);
+        score = Math.max(score, si.score);
+        if (subInstances.size() > 5) {
+            int pivot = 0;
+            for (int i = 1; i < subInstances.size(); ++i) {
+                if (subInstances.get(i).score < subInstances.get(pivot).score) {
+                    pivot = i;
+                }
+            }
+            subInstances.set(pivot, subInstances.get(subInstances.size() - 1));
+            subInstances.remove(subInstances.size() - 1);
+        }
+    }
+}
