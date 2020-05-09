f651f5cdf5a018ba3cc1bf88da559963f157125b
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-05-09 15:24:13 +0200

    update
    Former-commit-id: 396eee0b971a85169b41a26bf1569b09b9e3fa6b

--- src/server/table/experimental/ExplorerServer.java
+++ src/server/table/experimental/ExplorerServer.java
@@ -8,20 +8,24 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
 import server.table.experimental.gui.search.SearchHandler;
+import server.table.experimental.gui.search.SocketSearchServlet;
+import server.text.handler.WikiImageHandler;
 
 @Deprecated
 public class ExplorerServer {
     public static final String SEARCH_PATH = "/search";
     public static final String ENTITY_PATH = "/entity";
+    public static final String SOCKET_SEARCH_PATH = "/search_socket";
     public static final String TYPE_SUGGESTION_PATH = "/type_suggest";
+    public static final String WIKI_IMG_PATH = "/wikilink";
+    public static final String WIKI_VIEW_PATH = "/wikiview";
     public static final int DEFAULT_PORT = Integer.parseInt(Configuration.get("search.server.default-port"));
 
-    // [DEV|PRODUCTION]
     public static void main(String[] args) throws Exception {
-//        args = "DEV".split(" ");
         Server server = new Server(new QueuedThreadPool(32, 16));
 
         ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory());
@@ -32,18 +36,25 @@ public class ExplorerServer {
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setDirectoriesListed(true);
 
+        ServletContextHandler searchSocketHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        searchSocketHandler.addServlet(SocketSearchServlet.class, SOCKET_SEARCH_PATH);
+
         ContextHandler searchHandler = new ContextHandler();
         searchHandler.setContextPath(SEARCH_PATH);
-        searchHandler.setHandler(new SearchHandler(100));
+        searchHandler.setHandler(new SearchHandler(10));
 
-        ContextHandler typeSuggestionHandler = new ContextHandler();
-        typeSuggestionHandler.setContextPath(TYPE_SUGGESTION_PATH);
-        TypeSuggestionHandler.load(10);
-        typeSuggestionHandler.setHandler(new TypeSuggestionHandler(10));
+        ContextHandler wikiImgHandler = new ContextHandler();
+        wikiImgHandler.setContextPath(WIKI_IMG_PATH);
+        wikiImgHandler.setHandler(new WikiImageHandler());
 
-        ContextHandler entityHandler = new ContextHandler();
-        entityHandler.setContextPath(ENTITY_PATH);
-        entityHandler.setHandler(new EntityQfactHandler());
+//        ContextHandler typeSuggestionHandler = new ContextHandler();
+//        typeSuggestionHandler.setContextPath(TYPE_SUGGESTION_PATH);
+//        TypeSuggestionHandler.load(10);
+//        typeSuggestionHandler.setHandler(new TypeSuggestionHandler(10));
+
+//        ContextHandler entityHandler = new ContextHandler();
+//        entityHandler.setContextPath(ENTITY_PATH);
+//        entityHandler.setHandler(new EntityQfactHandler());
 
         WebAppContext ctx = new WebAppContext();
         ctx.setResourceBase("./web/");
@@ -56,8 +67,9 @@ public class ExplorerServer {
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{
                 searchHandler,
-                typeSuggestionHandler,
-                entityHandler,
+                searchSocketHandler,
+//                typeSuggestionHandler,
+//                entityHandler,
                 ctx});
 
         server.setHandler(handlers);
