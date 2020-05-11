8ee4e70391c8fb0f78874c53a210bb8bf8a63599
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-05-11 11:09:48 +0200

    update
    Former-commit-id: dbcc0a2ae9c3d53937f2843d97d08f53b6ebe63f

---
+++ src/server/common/handler/ResultCacheHandler.java
@@ -0,0 +1,35 @@
+package server.common.handler;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.apache.commons.lang.RandomStringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ResultCacheHandler extends HttpServlet {
+    private static Cache<String, String> CACHE = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+            .build();
+
+    // return session key
+    public static String addResult(String r) {
+        String key = RandomStringUtils.randomAlphanumeric(1024);
+        CACHE.put(key, r);
+        return key;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletResponse.setCharacterEncoding("utf-8");
+        httpServletResponse.getWriter().print(CACHE.getIfPresent(request.getParameter("s")));
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
