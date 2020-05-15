f47b41fcfafbe7a7d10e776f00d2c901c1dfaa56
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-05-16 00:59:28 +0200

    up
    Former-commit-id: aec406ceb4590ab2d14fb0d9148a8f5b1755816e

--- run_no_notification.sh
+++ run_no_notification.sh
@@ -3,4 +3,4 @@
 # Ex: run_no_notification.sh 8G <MainClass> <args...>
 
 args="${@:3}"
-export MAVEN_OPTS="-Xms8G -Xmx$1 -XX:ParallelGCThreads=4 -XX:+UseG1GC -XX:MaxHeapFreeRatio=30 -XX:MinHeapFreeRatio=10 -XX:+PrintFlagsFinal" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args"
+export MAVEN_OPTS="-Xms8G -Xmx$1 -XX:ParallelGCThreads=4 -XX:MaxHeapFreeRatio=30 -XX:MinHeapFreeRatio=10 -XX:+PrintFlagsFinal" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args"
