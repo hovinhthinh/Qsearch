e21ca7ce975ffdf9255618ed4a526c65b96699be
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-05-28 16:08:53 +0200

    update

--- src/model/quantity/QuantityConstraint.java
+++ src/model/quantity/QuantityConstraint.java
@@ -7,9 +7,12 @@ import model.query.SimpleQueryParser;
 import nlp.NLP;
 import util.Pair;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 // Not working: "higher than 1 million euros"
@@ -24,6 +27,19 @@ public class QuantityConstraint {
     public String fineGrainedDomain;
     public String phrase;
 
+    // return first one if available, null otherwise
+    private static edu.illinois.cs.cogcomp.quant.standardize.Quantity getQuantityFromStr(String str) {
+        try {
+            for (QuantSpan span : QUANTIFIER_LOCAL.get().getSpans("This quantity is " + str + " .", true, null)) {
+                if (span.object instanceof edu.illinois.cs.cogcomp.quant.standardize.Quantity) {
+                    return (edu.illinois.cs.cogcomp.quant.standardize.Quantity) span.object;
+                }
+            }
+        } catch (IndexOutOfBoundsException | AnnotatorException e) {
+        }
+        return null;
+    }
+
     public static QuantityConstraint parseFromString(String constraintString) {
         constraintString = SimpleQueryParser.preprocess(constraintString);
 
@@ -33,6 +49,7 @@ public class QuantityConstraint {
 
         try {
             boolean flag = false;
+            loop:
             for (QuantSpan span : QUANTIFIER_LOCAL.get().getSpans("This quantity is " + constraintString + " .", true, null)) { // get last one
                 if (span.object instanceof edu.illinois.cs.cogcomp.quant.standardize.Quantity) {
                     edu.illinois.cs.cogcomp.quant.standardize.Quantity q =
@@ -44,7 +61,28 @@ public class QuantityConstraint {
                     c.resolutionCodeExplicitlyGiven = resolution.second;
                     c.domain = QuantityDomain.getDomain(c.quantity);
                     c.fineGrainedDomain = QuantityDomain.getFineGrainedDomain(c.quantity);
-                    flag = true;
+
+                    if (c.resolutionCode == QuantityResolution.Value.RANGE) {
+                        // parse the first value
+                        c.quantity.value2 = c.quantity.value;
+                        q.phrase = q.phrase.trim();
+                        for (Pattern p : QuantityResolution.RANGE_SIGNAL) {
+                            Matcher m = Pattern.compile(p.pattern() + "\\s+" + Pattern.quote(q.phrase)).matcher(constraintString);
+                            if (m.find()) {
+                                String v1Span = m.group();
+                                v1Span = v1Span.substring(0, v1Span.length() - q.phrase.length()).trim();
+                                v1Span = v1Span.substring(v1Span.indexOf(' '), v1Span.lastIndexOf(' ')).trim();
+                                edu.illinois.cs.cogcomp.quant.standardize.Quantity v1 = getQuantityFromStr(v1Span);
+                                if (v1 != null) {
+                                    c.quantity.value = v1.value;
+                                    flag = true;
+                                    break loop;
+                                }
+                            }
+                        }
+                    } else {
+                        flag = true;
+                    }
                 }
             }
             if (flag) {
@@ -56,9 +94,13 @@ public class QuantityConstraint {
     }
 
     public static void main(String[] args) {
+        System.out.println(parseFromString("from 140-201 km").toString());
+        System.out.println(parseFromString("between 100 million and 200 billion mpg").toString());
         System.out.println(parseFromString("more than 30 km long").toString());
-        System.out.println(parseFromString("more than $100 million dollars").match(Quantity.fromQuantityString(
-                "(22600000.000;US$;=)")));
+
+        System.out.println(parseFromString("more than $100 million dollars").match(
+                Quantity.fromQuantityString("(22600000.000;US$;=)")));
+        System.out.println(parseFromString("from 140 to 201 km").match(Quantity.fromQuantityString("(150000;m;=)")));
     }
 
     public boolean match(Quantity q) {
@@ -75,6 +117,9 @@ public class QuantityConstraint {
             return Math.abs(qValue - thisQValue) <= 1e-3;
         } else if (resolutionCode == QuantityResolution.Value.APPROXIMATE) {
             return Math.abs(qValue - thisQValue) <= Math.abs(thisQValue * QuantityResolution.APPROXIMATE_RATE);
+        } else if (resolutionCode == QuantityResolution.Value.RANGE) {
+            double thisQValue2 = quantity.value2 * QuantityDomain.getScale(quantity);
+            return qValue >= thisQValue && qValue <= thisQValue2;
         } else {
             throw new RuntimeException("Unknown resolution.");
         }
@@ -100,10 +145,16 @@ public class QuantityConstraint {
         private static final LinkedHashSet<String> EXACT_SIGNAL = new LinkedHashSet<>(Arrays.asList(
                 "exactly", "exact"
         ));
+        private static final ArrayList<Pattern> RANGE_SIGNAL = new ArrayList<Pattern>() {{
+            add(Pattern.compile("\\bbetween\\b.*?\\band\\b"));
+            add(Pattern.compile("\\bfrom\\b.*?\\bto\\b"));
+        }};
+
         // TODO: RANGE, OTHER_THAN
 
         // map signal to standard signal
         public static HashMap<String, String> ALL_SIGNALS = new HashMap<>();
+
         static {
             for (LinkedHashSet<String> signalSet : Arrays.asList(UPPER_BOUND_SIGNAL, LOWER_BOUND_SIGNAL, EXACT_SIGNAL)) {
                 String mainSignal = signalSet.iterator().next();
@@ -135,6 +186,11 @@ public class QuantityConstraint {
                     return new Pair<>(Value.EXACT, true);
                 }
             }
+            for (Pattern p : RANGE_SIGNAL) {
+                if (p.matcher(constraintString).find()) {
+                    return new Pair<>(Value.RANGE, true);
+                }
+            }
 
             return new Pair<>(
                     Math.abs(quantity.value) <= APPROXIMATE_THRESHOLD ? Value.EXACT : Value.APPROXIMATE,
@@ -144,8 +200,7 @@ public class QuantityConstraint {
 
         // Now handle only these resolutions.
         public enum Value {
-            LOWER_BOUND, UPPER_BOUND, EXACT, APPROXIMATE
+            LOWER_BOUND, UPPER_BOUND, EXACT, APPROXIMATE, RANGE
         }
-
     }
 }
