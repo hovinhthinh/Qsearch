2c8391f54e88434d87a7d565c91463472a90bc35
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-19 11:53:12 +0200

    update
    Former-commit-id: ed639ccc62f75d1ede69a4164272988f2db85473

--- src/server/table/experimental/EntityQfactHandler.java
+++ src/server/table/experimental/EntityQfactHandler.java
@@ -1,36 +1,42 @@
 package server.table.experimental;
 
 import nlp.NLP;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.json.JSONObject;
+import server.common.handler.ResultCacheHandler;
 import server.table.QfactLight;
 import server.table.TableQfactLoader;
+import storage.table.index.TableIndex;
+import storage.table.index.TableIndexStorage;
 import util.Gson;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
-@Deprecated
-public class EntityQfactHandler extends AbstractHandler {
+class BrowsingResult {
+    public ArrayList<List<QfactLight>> result = new ArrayList<>();
+    public HashMap<String, TableIndex> tableId2Index = new HashMap<>();
+}
+
+public class EntityQfactHandler extends HttpServlet {
     public static final Logger LOGGER = Logger.getLogger(EntityQfactHandler.class.getName());
 
     static ArrayList<QfactLight> qfacts = TableQfactLoader.load();
 
     @Override
-    public void handle(String s, Request request, HttpServletRequest httpServletRequest,
-                       HttpServletResponse httpServletResponse) throws IOException {
-        request.setHandled(true);
+    protected void doGet(HttpServletRequest request, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         httpServletResponse.setCharacterEncoding("utf-8");
 
         // Get parameters
         String entityConstraint = NLP.stripSentence(request.getParameter("entity")).toLowerCase();
-
-        ArrayList<List<QfactLight>> result = new ArrayList<>();
+        BrowsingResult r = new BrowsingResult();
         if (!entityConstraint.isEmpty()) {
             for (int i = 0; i < qfacts.size(); ++i) {
                 if (qfacts.get(i).entity.substring(5).replace('_', ' ').toLowerCase().contains(entityConstraint)) {
@@ -38,25 +44,32 @@ public class EntityQfactHandler extends AbstractHandler {
                     while (j < qfacts.size() - 1 && qfacts.get(j + 1).entity.equals(qfacts.get(j).entity)) {
                         ++j;
                     }
-                    result.add(qfacts.subList(i, j + 1));
+                    r.result.add(qfacts.subList(i, j + 1));
                     int pivot = 0;
-                    if (result.size() > 100) {
-                        for (int k = 1; k < result.size(); ++k) {
-                            if (result.get(k).size() < result.get(pivot).size()) {
+                    if (r.result.size() > 100) {
+                        for (int k = 1; k < r.result.size(); ++k) {
+                            if (r.result.get(k).size() < r.result.get(pivot).size()) {
                                 pivot = k;
                             }
                         }
-                        result.set(pivot, result.get(result.size() - 1));
-                        result.remove(result.size() - 1);
+                        r.result.set(pivot, r.result.get(r.result.size() - 1));
+                        r.result.remove(r.result.size() - 1);
                     }
                     i = j;
                 }
             }
         }
-        Collections.sort(result, (o1, o2) -> Integer.compare(o2.size(), o1.size()));
+        Collections.sort(r.result, (o1, o2) -> Integer.compare(o2.size(), o1.size()));
+        for (List<QfactLight> l : r.result) {
+            for (QfactLight f : l) {
+                if (!r.tableId2Index.containsKey(f.tableId)) {
+                    r.tableId2Index.put(f.tableId, TableIndexStorage.get(f.tableId));
+                }
+            }
+        }
 
-        httpServletResponse.getWriter().print(Gson.toJson(result));
-
+        String sessionKey = ResultCacheHandler.addResult(Gson.toJson(r));
+        httpServletResponse.getWriter().print(new JSONObject().append("s", sessionKey).toString());
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
     }
 }
