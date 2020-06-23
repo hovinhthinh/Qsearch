9493f52240303cc3008af13e78bc668a99c50ef6
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-24 01:41:41 +0200

    update
    Former-commit-id: 971c4ba80a948e9c067c8abfa2993485a91b2bf4

--- src/util/distributed/MapInteractiveRunner.java
+++ src/util/distributed/MapInteractiveRunner.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MapInteractiveRunner {
     public static final String ON_READY = "__map_ready__";
@@ -16,6 +18,7 @@ public class MapInteractiveRunner {
 
     public static final int KEEP_ALIVE_INTERVAL = 10;
     public static final int GC_INTERVAL = 1000;
+    public static final int SELF_KILLING_LONG_PROCESSING_TIMEOUT = 3600;
 
     // args: <String2StringMapClass>
     public static void main(String[] args) {
@@ -30,12 +33,20 @@ public class MapInteractiveRunner {
         System.out.println(ON_READY);
         System.out.flush();
 
+        System.gc();
+
+        final AtomicBoolean isProcessingAnInput = new AtomicBoolean(false);
+        final AtomicLong lastInputTimestamp = new AtomicLong(-1);
+
         Thread keepAlive = new Thread(() -> {
             do {
                 try {
                     Thread.sleep(KEEP_ALIVE_INTERVAL * 1000);
                 } catch (InterruptedException e) {
                     break;
+                }
+                if (isProcessingAnInput.get() && System.currentTimeMillis() >= lastInputTimestamp.get() + SELF_KILLING_LONG_PROCESSING_TIMEOUT * 1000) {
+                    Runtime.getRuntime().halt(1);
                 }
                 synchronized (System.out) {
                     System.out.println(ON_KEEP_ALIVE);
@@ -45,7 +56,6 @@ public class MapInteractiveRunner {
         });
         keepAlive.start();
 
-        System.gc();
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
@@ -53,6 +63,8 @@ public class MapInteractiveRunner {
 
             long lastGCTime = System.currentTimeMillis();
             while ((str = in.readLine()) != null) {
+                lastInputTimestamp.set(System.currentTimeMillis());
+                isProcessingAnInput.set(true);
                 List<String> output = null;
                 try {
                     output = mapper.map(str);
@@ -76,6 +88,7 @@ public class MapInteractiveRunner {
                     System.out.println(String.format("%s\t%s", ON_OUTPUT, arr.toString()));
                     System.out.flush();
                 }
+                isProcessingAnInput.set(false);
             }
             mapper.after();
         } catch (IOException e) {
