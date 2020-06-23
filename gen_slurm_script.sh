f3102575dff0d1acd74ceb1183a24dd7b4db5857
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-24 01:04:52 +0200

    update

---
+++ gen_slurm_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Generate a slurm script, which calls the run_map_parallel.sh script.
+# gen_slurm_script.sh <nProcesses> <MapClass> <input> <output> <slurm_output/nohup.out>
+
+args="${@:1:4}"
+slurm_output=`realpath $5`
+echo "#!/bin/bash
+#SBATCH -p cpu20
+#SBATCH -t 2-00:00:00
+#SBATCH -o ${slurm_output}
+
+cd /home/hvthinh/TabQs/ && ./run_map_parallel.sh $args"
+
+
