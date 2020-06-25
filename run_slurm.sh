8fedeefa4c2181b8fde3edeaf42ce7bf02c76074
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-25 23:03:28 +0200

    update

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,8 +5,6 @@ echo "Split data"
 islices="$3.slices" && mkdir ${islices}
 ./split.sh $3 $1 ${islices}
 
-slurm_output=`realpath $4`.slices && mkdir ${slurm_output}
-
 temp_script=$(mktemp)
 echo "Temporary slurm script is generated at: ${temp_script}"
 
