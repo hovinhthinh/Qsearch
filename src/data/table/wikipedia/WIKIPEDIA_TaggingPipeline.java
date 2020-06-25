1a192cfe7e103b66c91ff9aa35a598166ade04fe
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-25 21:43:08 +0200

    update
    Former-commit-id: ac44ca6bd186e3a850183290f50ff228779b729e

--- src/data/table/wikipedia/WIKIPEDIA_TaggingPipeline.java
+++ src/data/table/wikipedia/WIKIPEDIA_TaggingPipeline.java
@@ -3,12 +3,9 @@ package data.table.wikipedia;
 import model.table.Table;
 import nlp.Static;
 import pipeline.table.*;
-import util.FileUtils;
 import util.Gson;
-import util.SelfMonitor;
 import util.distributed.String2StringMap;
 
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -44,31 +41,6 @@ public class WIKIPEDIA_TaggingPipeline extends String2StringMap {
         );
     }
 
-    // Args: <input> <output>
-    public static void main(String[] args) {
-//        args = "/local/home/hvthinh/datasets/TabEL.json.shuf.gz /local/home/hvthinh/datasets/TabEL.json.shuf.out.gz".split("\\s++");
-
-        PrintWriter out = FileUtils.getPrintWriter(args[1], "UTF-8");
-        FileUtils.LineStream stream = FileUtils.getLineStream(args[0], "UTF-8");
-
-        WIKIPEDIA_TaggingPipeline p = new WIKIPEDIA_TaggingPipeline();
-
-        SelfMonitor m = new SelfMonitor(WIKIPEDIA_TaggingPipeline.class.getName(), -1, 60);
-        m.start();
-        for (String line : stream) {
-            m.incAndGet();
-            List<String> result = p.map(line);
-            if (result == null) {
-                continue;
-            }
-            for (String r : result) {
-                out.println(r);
-            }
-        }
-        m.forceShutdown();
-        out.close();
-    }
-
     @Override
     public List<String> map(String input) {
         Table table = WIKIPEDIA.parseFromJSON(input); // already contains Entity Tags
@@ -76,5 +48,9 @@ public class WIKIPEDIA_TaggingPipeline extends String2StringMap {
             return null;
         }
         return Arrays.asList(Gson.toJson(table));
+    }
+
+    static {
+//        ARGS = "/local/home/hvthinh/datasets/TabEL.json.shuf.gz /local/home/hvthinh/datasets/TabEL.json.shuf.out.gz".split("\\s++");
     }
 }
