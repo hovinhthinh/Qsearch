51ef9a2195fce1c9fcefa3ac638b42b07d8c32cd
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-06-28 23:56:34 +0200

    update
    Former-commit-id: 1fd4ee8c14c9f77bafdc19f875b084086dbfcee6

--- run_no_notification.sh
+++ run_no_notification.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # run.sh <Xmx> <MainClass> <args...>
 # Ex: run_no_notification.sh 12G <MainClass> <args...>
+# This script is used for parallel running only, so do NOT touch this.
 
 args="${@:3}"
-export MAVEN_OPTS="-Xmx$1 -XX:ParallelGCThreads=4" && mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args"
+export MAVEN_OPTS="-Xms$1 -Xmx$1 -XX:+UseParallelOldGC -XX:GCTimeRatio=19 -XX:ParallelGCThreads=4" && \
+  mvn exec:java -Dexec.classpathScope=compile -Dexec.mainClass="$2" -Dexec.args="$args"
