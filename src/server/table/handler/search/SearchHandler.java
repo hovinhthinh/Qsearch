5d33edde5e2cbd3861780184e0e45dbfb62427bc
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-07-09 22:20:57 +0200

    update
    Former-commit-id: f6d8675ad5bc5a0d2d4810db392755330a98f3e9 [formerly 7efe912eea0ca8c6f31a795a9a8856f52a11169b]
    Former-commit-id: 3a6982b556771cf7022bc768038ed15ce6b6d4f4

--- src/server/table/handler/search/SearchHandler.java
+++ src/server/table/handler/search/SearchHandler.java
@@ -3,8 +3,11 @@ package server.table.handler.search;
 import model.quantity.QuantityConstraint;
 import model.query.SimpleQueryParser;
 import nlp.NLP;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import pipeline.table.QfactTaxonomyGraph;
 import server.common.handler.ResultCacheHandler;
+import server.table.QfactLight;
 import server.table.ResultInstance;
 import server.table.TableQuery;
 import storage.table.index.TableIndexStorage;
@@ -24,6 +27,8 @@ import java.util.logging.Logger;
 
 public class SearchHandler extends HttpServlet {
     public static final Logger LOGGER = Logger.getLogger(SearchHandler.class.getName());
+
+    public static final HashMap<Integer, QfactTaxonomyGraph.EntityTextQfact> BG_TEXT_QFACT_MAP = QfactTaxonomyGraph.loadBackgroundTextQfactMap();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse httpServletResponse) throws ServletException, IOException {
@@ -117,6 +122,19 @@ public class SearchHandler extends HttpServlet {
                         for (ResultInstance.SubInstance si : ri.subInstances) {
                             if (!response.tableId2Index.containsKey(si.qfact.tableId)) {
                                 response.tableId2Index.put(si.qfact.tableId, TableIndexStorage.get(si.qfact.tableId));
+                            }
+                            if (si.qfact.explainQfactIds != null && !si.qfact.explainQfactIds.equals("null")) {
+                                si.qfact = (QfactLight) si.qfact.clone(); // IMPORTANT!
+
+                                JSONArray bgQfactIds = new JSONArray(si.qfact.explainQfactIds);
+                                StringBuilder sb = new StringBuilder();
+                                for (int i = 0; i < bgQfactIds.length(); ++i) {
+                                    if (sb.length() > 0) {
+                                        sb.append("\r\n");
+                                    }
+                                    sb.append(BG_TEXT_QFACT_MAP.get(bgQfactIds.getInt(i)).toString());
+                                }
+                                si.qfact.explainStr = sb.toString();
                             }
                         }
                     }
