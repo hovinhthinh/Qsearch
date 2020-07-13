a53ad0377b896b3835f793b44e61474b4c44033e
Ho Vinh Thinh <hovinhthinh@gmail.com>
2020-07-13 16:49:33 +0200

    update
    Former-commit-id: 9a296ecf90c11335c25fd150261c9b460ed43952 [formerly 202f38254e0925c4fdd2743d96194d94e675334f]
    Former-commit-id: 5c60514fc95bd832865f17eb2d3fdf96e23423dc

--- src/server/common/handler/WikiViewHandler.java
+++ src/server/common/handler/WikiViewHandler.java
@@ -11,17 +11,26 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URLEncoder;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WikiViewHandler extends HttpServlet {
-    private static final String URL_TEMPLATE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{ENTITY}/monthly/2020050100/2020053100";
+    private static final String URL_TEMPLATE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{ENTITY}/monthly/2019010100/2019123100";
     private static final Proxy p = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("dmz-gw.mpi-klsb.mpg.de", 3128));
     private static final boolean USE_PROXY = false;
 
+    private static ConcurrentHashMap<String, Integer> CACHE = new ConcurrentHashMap<>();
+
     public static int getView(String entity) {
+        Integer v = CACHE.get(entity);
+        if (v != null) {
+            return v;
+        }
         try {
-            String url = URL_TEMPLATE.replace("{ENTITY}", URLEncoder.encode(entity, "UTF-8"));
+            String url = URL_TEMPLATE.replace("{ENTITY}", URLEncoder.encode(entity.substring(1, entity.length() - 1), "UTF-8"));
             String content = Crawler.getContentFromUrl(url, USE_PROXY ? p : null);
-            return new JSONObject(content).getJSONArray("items").getJSONObject(0).getInt("views");
+            v = new JSONObject(content).getJSONArray("items").getJSONObject(0).getInt("views");
+            CACHE.put(entity, v);
+            return v;
         } catch (Exception e) {
             return -1;
         }
@@ -31,15 +40,12 @@ public class WikiViewHandler extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         httpServletResponse.setCharacterEncoding("utf-8");
         String entity = request.getParameter("entity");
-        if (entity.length() >= 2) {
-            entity = entity.substring(1, entity.length() - 1);
-        }
         JSONObject response = new JSONObject().put("view", getView(entity));
         httpServletResponse.getWriter().print(response.toString());
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
     }
 
     public static void main(String[] args) {
-        System.out.println(getView("Barack_Obama"));
+        System.out.println(getView("<Barack_Obama>"));
     }
 }
