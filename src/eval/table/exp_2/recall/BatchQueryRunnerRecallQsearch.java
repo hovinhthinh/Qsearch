ac65acd7acd979db53529c36dc5ec71cc47ebb0a
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-08-07 23:20:17 +0200

    update
    Former-commit-id: 39666186c9591b1a31e92c39e7492042b18d09c9

---
+++ src/eval/table/exp_2/recall/BatchQueryRunnerRecallQsearch.java
@@ -0,0 +1,90 @@
+package eval.table.exp_2.recall;
+
+import com.google.common.util.concurrent.AtomicDouble;
+import org.json.JSONObject;
+import server.table.handler.search.SearchResult;
+import shaded.org.apache.http.client.utils.URIBuilder;
+import util.*;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BatchQueryRunnerRecallQsearch {
+    public static final String END_POINT = "http://halimede:6993";
+
+    public static void run(String[] args) {
+        final File outputFolder = args.length > 1 ? new File(args[1]) : null;
+        if (outputFolder != null) {
+            outputFolder.mkdirs();
+        }
+
+        FileUtils.LineStream stream = FileUtils.getLineStream(args[0], "UTF-8");
+        SelfMonitor monitor = new SelfMonitor(BatchQueryRunnerRecallQsearch.class.getName(), -1, 30);
+        monitor.start();
+        Crawler.READ_TIME_OUT = 3000 * 1000;
+        final AtomicDouble map = new AtomicDouble(0), mrr = new AtomicDouble(0), recall = new AtomicDouble(0);
+        final AtomicInteger cnt = new AtomicInteger(0);
+        Concurrent.runAndWait(() -> {
+            while (true) {
+                String line;
+                synchronized (stream) {
+                    line = stream.readLine();
+                }
+                if (line == null) {
+                    break;
+                }
+                RecallQuery q = Gson.fromJson(line, RecallQuery.class);
+                ArrayList<String> groundtruth = new ArrayList<>();
+                for (GroundFact f : q.groundFacts) {
+                    groundtruth.add(f.entity);
+                }
+                try {
+                    URIBuilder b = new URIBuilder(END_POINT + "/search");
+                    b.addParameter("corpus", "[\"STICS\",\"NYT\",\"WIKIPEDIA\"]");
+                    b.addParameter("ntop", q.groundFacts.size() + "");
+                    b.addParameter("model", "EMBEDDING");
+                    b.addParameter("full", q.full + "");
+                    b.addParameter("lambda", "0.1");
+                    b.addParameter("alpha", "3");
+                    b.addParameter("groundtruth", Gson.toJson(groundtruth));
+
+                    String session = new JSONObject(Crawler.getContentFromUrl(b.toString())).getString("s");
+
+                    b = new URIBuilder(END_POINT + "/session");
+                    b.addParameter("s", session);
+                    SearchResult r = Gson.fromJson(Crawler.getContentFromUrl(b.toString()), SearchResult.class);
+
+                    map.addAndGet(r.AP);
+                    mrr.addAndGet(r.RR);
+                    recall.addAndGet(r.RECALL);
+                    cnt.incrementAndGet();
+                    if (outputFolder != null) {
+                        PrintWriter out = FileUtils.getPrintWriter(new File(outputFolder, r.encode()), StandardCharsets.UTF_8);
+                        out.println(Gson.toJson(r));
+                        out.close();
+                    }
+                    monitor.incAndGet();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println("Err: " + line);
+                }
+            }
+        }, 6);
+
+        monitor.forceShutdown();
+        System.out.println(String.format("MAP: %.3f    MRR: %.3f    RECALL: %.3f",
+                map.get() / cnt.get(), mrr.get() / cnt.get(), recall.get() / cnt.get()));
+        System.out.flush();
+
+    }
+
+    // args: <inputFile> in format query[\tdomain]; <output-folder>
+    public static void main(String[] args) throws Exception {
+        args = "eval/table/exp_2/recall/recall_query.json eval/table/exp_2/annotation-recall-qsearch".split(" ");
+        run(args);
+    }
+}
+
