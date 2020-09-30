9ad1c7ee5886783ea0b82c018183c4a81d3dbdeb
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-09-30 09:11:37 +0200

    update
    Former-commit-id: e0c85a947cdbec18e571b198ccf2ca73670eaffa

--- src/server/common/handler/WikiViewHandler.java
+++ src/server/common/handler/WikiViewHandler.java
@@ -1,5 +1,6 @@
 package server.common.handler;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import util.Crawler;
 
@@ -14,12 +15,13 @@ import java.net.URLEncoder;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WikiViewHandler extends HttpServlet {
-    private static final String URL_TEMPLATE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{ENTITY}/monthly/2019010100/2019123100";
+    private static final String URL_TEMPLATE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{ENTITY}/monthly/20190101/20191231";
     private static final Proxy p = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("dmz-gw.mpi-klsb.mpg.de", 3128));
     private static final boolean USE_PROXY = false;
 
     private static ConcurrentHashMap<String, Integer> CACHE = new ConcurrentHashMap<>();
 
+    // returns: -2: article not found; -1: unknown error (e.g. network error, timeout, ...)
     public static int getView(String entity) {
         Integer v = CACHE.get(entity);
         if (v != null) {
@@ -27,8 +29,18 @@ public class WikiViewHandler extends HttpServlet {
         }
         try {
             String url = URL_TEMPLATE.replace("{ENTITY}", URLEncoder.encode(entity.substring(1, entity.length() - 1), "UTF-8"));
-            String content = Crawler.getContentFromUrl(url, USE_PROXY ? p : null);
-            v = new JSONObject(content).getJSONArray("items").getJSONObject(0).getInt("views");
+            String content = Crawler.getContentFromUrl(url, null, "GET", null, USE_PROXY ? p : null, true);
+
+            JSONObject json = new JSONObject(content);
+            if (json.has("title") && json.getString("title").equals("Not found.")) {
+                return -2;
+            }
+
+            JSONArray monthly = json.getJSONArray("items");
+            v = 0;
+            for (int i = 0; i < monthly.length(); ++i) {
+                v += monthly.getJSONObject(i).getInt("views");
+            }
             CACHE.put(entity, v);
             return v;
         } catch (Exception e) {
