04e3790b12ac131d4960fbb2801d1232d22a0a76
hvthinh <hvthinh@mpi-inf.mpg.de>
2020-10-01 17:26:14 +0200

    update
    Former-commit-id: 339913d0a18497bd504aa4273de5977eb7e7b7e4

--- src/server/table/TableQuery.java
+++ src/server/table/TableQuery.java
@@ -30,7 +30,7 @@ public class TableQuery {
     public static double SAME_ROW_MATCH_WEIGHT = 0.9; // old: 0.85
     public static double RELATED_TEXT_MATCH_WEIGHT = 0.8;
 
-    public static double TOPIC_DRIFT_PENALTY = 1;
+    public static double TOPIC_DRIFT_PENALTY = 0.25;
 
     public static double QUANTITY_MATCH_WEIGHT = 0.03;
     public static double ENTITY_POPULARITY_WEIGHT = 0.045;
