0e0de0f4bdd5505a32d3050a5fca200e2e0de9cb
hvthinh <hvthinh@mpi-inf.mpg.de>
2021-01-29 13:52:24 +0100

    update

--- src/util/Gson.java
+++ src/util/Gson.java
@@ -1,12 +1,21 @@
 package util;
 
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
 public class Gson {
-    private static ThreadLocal<com.google.gson.Gson> GSON_LOCAL = ThreadLocal.withInitial(() -> new com.google.gson.Gson());
+    private static ThreadLocal<com.google.gson.Gson> GSON_LOCAL =
+            ThreadLocal.withInitial(() -> new com.google.gson.Gson());
+    private static ThreadLocal<com.google.gson.Gson> GSON_LOCAL_PRETTY =
+            ThreadLocal.withInitial(() -> new GsonBuilder().setPrettyPrinting().create());
 
     public static String toJson(Object src) {
-        return GSON_LOCAL.get().toJson(src);
+        return toJson(src, false);
+    }
+
+    public static String toJson(Object src, boolean pretty) {
+        com.google.gson.Gson g = (pretty ? GSON_LOCAL_PRETTY : GSON_LOCAL).get();
+        return g.toJson(src);
     }
 
     public static <T> T fromJson(String json, Class<T> classOfT) throws JsonSyntaxException {
