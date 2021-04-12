5367cd118892bd64134e7177a78e15f48341fc63
hvthinh <hvthinh@mpi-inf.mpg.de>
2021-04-12 16:24:53 +0200

    update

--- src/model/text/tag/TimeTag.java
+++ src/model/text/tag/TimeTag.java
@@ -1,6 +1,7 @@
 package model.text.tag;
 
 public class TimeTag extends Tag {
+    public boolean isRelative; // need a reference time to fully resolve the time.
     // start (inclusive), end (inclusive) time values (unix time).
     // The granularity is day.
     public long rangeFrom, rangeTo;
@@ -11,5 +12,15 @@ public class TimeTag extends Tag {
 
         this.rangeFrom = rangeFrom;
         this.rangeTo = rangeTo;
+        this.isRelative = false;
+    }
+
+    public TimeTag(int beginIndex, int endIndex, boolean isRelative) {
+        super(beginIndex, endIndex);
+        PLACEHOLDER = Placeholder.TIME;
+
+        this.rangeFrom = -1;
+        this.rangeTo = -1;
+        this.isRelative = isRelative;
     }
 }
