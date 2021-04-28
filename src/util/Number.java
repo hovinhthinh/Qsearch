9c96af6ac86588ac90f4e18b7e8daf3743e4b14c
hvthinh <hvthinh@mpi-inf.mpg.de>
2021-04-28 14:59:19 +0200

    minor fix

--- src/util/Number.java
+++ src/util/Number.java
@@ -3,7 +3,7 @@ package util;
 public class Number {
 
     public static double relativeNumericDistance(double a, double b) {
-        if (a == 0 && b == 0) {
+        if (Math.abs(a - b) <= Constants.EPS) {
             return 0;
         }
         return Math.abs(a - b) / Math.max(Math.abs(a), Math.abs(b));
