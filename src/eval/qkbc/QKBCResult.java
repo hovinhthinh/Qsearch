25849bb385a7a56ee05e4958240197e207e93d6f
hvthinh <hvthinh@mpi-inf.mpg.de>
2021-07-18 11:57:24 +0200

    update

--- src/eval/qkbc/QKBCResult.java
+++ src/eval/qkbc/QKBCResult.java
@@ -24,7 +24,7 @@ public class QKBCResult {
 
         System.out.println(inputFile);
         System.out.println(r.ctxList.toString());
-        System.out.println(String.format("%12s%12s%12s%16s%16s%12s", "iter", "#facts", "prec.", "recall(x1e-2)", "ext.", "#noise"));
+        System.out.println(String.format("%12s%12s%12s%12s%16s%16s%12s", "iter", "#facts", "prec.@cwa", "prec.", "rec.@cwa|x1e-2", "ext.", "#noise"));
         int it = 0;
         do {
             int currentIt = ++it;
@@ -43,26 +43,33 @@ public class QKBCResult {
 
             // recall, ext (only when refinement by time == false)
             double recall = -1;
+            double precCWA = -1;
             int ext = -1;
             if (r.groundTruthSize != null) {
                 int nTrue = 0;
+                int nInGroundTruth = 0;
+
                 ext = 0;
                 loop:
                 for (RelationInstance ri : currentEffectiveInstances) {
                     if (ri.groundtruth == null) {
                         ext++;
                         continue;
-                    } else if (ri.groundtruth) {
-                        ++nTrue;
+                    } else {
+                        ++nInGroundTruth;
+                        if (ri.groundtruth) {
+                            ++nTrue;
+                        }
                     }
                 }
                 recall = 1.0 * nTrue / r.groundTruthSize * 100;
+                precCWA = nInGroundTruth == 0 ? -1 : 1.0 * nTrue / nInGroundTruth;
             }
 
             // TODO: prec
             double prec = -1;
 
-            System.out.println(String.format("%12d%12d%12.3f%16.3f%16s%12d", it, nFacts, prec, recall,
+            System.out.println(String.format("%12d%12d%12.3f%12.3f%16.3f%16s%12d", it, nFacts, precCWA, prec, recall,
                     ext == -1 ? String.format("%.3f", 1.0 * ext) : String.format("%d(%.3f)", ext, 1.0 * ext / currentEffectiveInstances.size()),
                     currentNoise.size()
                     ));
