de341c2e4f2eef851a2ee1336f860b0bd30d2fcd
Ho Vinh Thinh <hovinhthinh@gmail.com>
2022-07-20 15:41:08 +0200

    clean

--- mem.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ps -U hvthinh --no-headers -o rss | awk '{sum+=$1} END {print int(sum/1024/1024) "GB"}'
--- run_hadoop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# args: <mapperClass>, <inputOnHDFS>, <outputOnHDFS>
-mvn clean package
-hadoop jar target/tabqs-1.0-SNAPSHOT.jar util.distributed.hadoop.MapOnlyJob -Dmapreduce.map.memory.mb=12288 $1 $2 $3
