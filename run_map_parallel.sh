3e6f1204720211f0387284e192aae10a0d20853b
Ho Vinh Thinh <hovinhthinh@gmail.com>
2022-07-22 16:45:52 +0200

    clean

--- run_map_parallel.sh
+++ run_map_parallel.sh
@@ -1,47 +1,8 @@
 #!/usr/bin/env bash
 # Use to run a task to process a input file with multithreading. Each element of the input file and output file should be in a single line.
 # run_map_parallel.sh <nProcesses> <MapClass> <input> <output>
-
-textifyDuration() {
-   local duration=$1
-   local shiff=$duration
-   local secs=$((shiff % 60));  shiff=$((shiff / 60));
-   local mins=$((shiff % 60));  shiff=$((shiff / 60));
-   local hours=$shiff
-   local splur; if [ $secs  -eq 1 ]; then splur=''; else splur='s'; fi
-   local mplur; if [ $mins  -eq 1 ]; then mplur=''; else mplur='s'; fi
-   local hplur; if [ $hours -eq 1 ]; then hplur=''; else hplur='s'; fi
-   if [[ $hours -gt 0 ]]; then
-      txt="$hours hour$hplur, $mins minute$mplur, $secs second$splur"
-   elif [[ $mins -gt 0 ]]; then
-      txt="$mins minute$mplur, $secs second$splur"
-   else
-      txt="$secs second$splur"
-   fi
-   echo "Total time: $txt"
-}
-
-start_time="$(TZ=UTC0 printf '%(%s)T\n' '-1')"
-# START JOB
-
 args="${@:1}"
 export MAVEN_OPTS="-Xmx8G -XX:+UseParallelOldGC -XX:ParallelGCThreads=4" && mvn exec:java \
     -Dexec.classpathScope=compile \
     -Dexec.mainClass=util.distributed.ParallelMapClient \
     -Dexec.args="12G $args"
-
-# END JOB
-
-# Send notification
-running_time=$(( $(TZ=UTC0 printf '%(%s)T\n' '-1') - start_time ))
-running_time=`textifyDuration $running_time`
-hostname=`hostname`
-pwd=`pwd`
-command="${@:0}"
-( printf '%s\n' "Dear ${USER},"
-  printf '\n%s\n' "Your command is done:"
-  printf '\n     %s\n' "$hostname:$pwd\$ $command"
-  printf '\n%s\n' "$running_time"
-  printf '\n%s\n' "Cheers,"
-  printf '%s\n' "Your mail bot"
-) | mail -s "[$hostname] Your job is done" ${USER}@mpi-inf.mpg.de
--- src/eval/table/exp_2/BatchQueryRunner.java
+++ src/eval/table/exp_2/BatchQueryRunner.java
@@ -1,6 +1,7 @@
 package eval.table.exp_2;
 
-import org.json.JSONObject;
+import org.junit.Assert;
+import server.table.ResultInstance;
 import server.table.handler.search.SearchResult;
 import shaded.org.apache.http.client.utils.URIBuilder;
 import util.*;
@@ -8,20 +9,31 @@ import util.*;
 import java.io.File;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class BatchQueryRunner {
     public static final String END_POINT = "http://halimede:6993";
 
     // args: <inputFile> in format query[\tdomain]; <output-folder>
     public static void main(String[] args) throws Exception {
-        args = "eval/table/exp_2/queries_iswc.txt eval/table/exp_2/annotation-iswc-nolt-rescore/template".split(" ");
+        args = "eval/table/exp_2/queries_iswc.txt eval/table/exp_2/tmp/".split(" ");
+
+        String[] referFolders = new String[]{
+//                "eval/table/exp_2/annotation-iswc_(old)/evaluator-1",
+                "eval/table/exp_2/annotation-iswc-new",
+                "eval/table/exp_2/annotation-iswc-no-ct",
+                "eval/table/exp_2/annotation-iswc-no-sr",
+                "eval/table/exp_2/annotation-iswc-no-tt",
+                "eval/table/exp_2/annotation-iswc-no-rt",
+//                "eval/table/exp_2/annotation-iswc-no-rs_(old)/evaluator-1",
+        };
 
         File outputFolder = new File(args[1]);
         outputFolder.mkdirs();
         FileUtils.LineStream stream = FileUtils.getLineStream(args[0], "UTF-8");
         SelfMonitor monitor = new SelfMonitor(BatchQueryRunner.class.getName(), -1, 30);
         monitor.start();
-        Crawler.READ_TIME_OUT = 3000 * 1000;
+        Crawler.READ_TIME_OUT = 9000 * 1000;
         Concurrent.runAndWait(() -> {
             while (true) {
                 String line;
@@ -38,23 +50,62 @@ public class BatchQueryRunner {
                     b.addParameter("ntop", "10");
                     b.addParameter("linking-threshold", "0");
                     b.addParameter("full", arr[0]);
-                    b.addParameter("rescore", "true");
+                    b.addParameter("rescore", "false");
+
+//                    b.addParameter("TOPIC_DRIFT_PENALTY", "0");
+//                    b.addParameter("CAPTION_MATCH_WEIGHT", String.format("%.2f", 0.0));
+//                    b.addParameter("TITLE_MATCH_WEIGHT", String.format("%.2f", 0.0));
+//                    b.addParameter("DOM_HEADING_MATCH_WEIGHT", String.format("%.2f", 0.0));
+//                    b.addParameter("SAME_ROW_MATCH_WEIGHT", String.format("%.2f", 0.0));
+//                    b.addParameter("RELATED_TEXT_MATCH_WEIGHT", String.format("%.2f", 0.0));
+//                    b.addParameter("QUANTITY_MATCH_WEIGHT", String.format("%.2f", 0.0));
 
                     SearchResult r = Gson.fromJson(Crawler.getContentFromUrl(b.toString()), SearchResult.class);
 
                     if (arr.length > 1) {
                         r.evalDomain = arr[1];
                     }
+
+                    // Populate old annotation
+                    for (String rF : referFolders) {
+                        Assert.assertTrue(new File(rF).exists());
+                        File referFile = new File(rF, r.encode());
+
+                        if (referFile.exists()) {
+                            SearchResult rr = Gson.fromJson(FileUtils.getContent(referFile, StandardCharsets.UTF_8), SearchResult.class);
+                            HashMap<String, String> entity2Verdict = new HashMap<>();
+                            for (ResultInstance ri : rr.topResults) {
+                                if (ri.eval != null) {
+                                    entity2Verdict.put(ri.entity, ri.eval);
+                                }
+                            }
+                            for (ResultInstance ri : r.topResults) {
+                                if (ri.eval == null) {
+                                    ri.eval = entity2Verdict.get(ri.entity);
+                                } else {
+                                    String ground = entity2Verdict.get(ri.entity);
+                                    if (ground != null && !ground.equals(ri.eval)) {
+                                        System.err.println("IN-consistent: " + referFile.getAbsolutePath() + " --> " + ri.entity);
+                                    }
+                                }
+                            }
+                        } else {
+                            System.err.println("file not found: " + referFile.getAbsolutePath());
+                        }
+                    }
+                    // Done;
+
                     PrintWriter out = FileUtils.getPrintWriter(new File(outputFolder, r.encode()), StandardCharsets.UTF_8);
                     out.println(Gson.toJson(r));
                     out.close();
+                    System.out.println("DONE: " + r.fullQuery);
                     monitor.incAndGet();
                 } catch (Exception e) {
                     e.printStackTrace();
                     System.err.println("Err: " + line);
                 }
             }
-        }, 4);
+        }, 20);
 
         monitor.forceShutdown();
     }
